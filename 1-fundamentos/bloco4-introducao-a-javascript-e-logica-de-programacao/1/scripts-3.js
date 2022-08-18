03d9cf6ee73c367c6cafcb8feb5a14bb052d7c53
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-08-18 15:41:00 -0300

    Prática Condições If e Else

---
+++ 1-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/1/scripts-3.js
@@ -0,0 +1,10 @@
+const notaCandidato = 51;
+
+if (notaCandidato >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!");
+} else if (notaCandidato < 80 && notaCandidato >= 60) {
+    console.log("Você está na nossa lista de espera!");
+} else {
+    console.log("Você foi reprovado!");
+}
+
