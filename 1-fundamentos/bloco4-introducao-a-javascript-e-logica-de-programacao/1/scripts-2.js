ad1a36b4a52319e1a10352e4b1a55ac0cf38de11
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-08-18 15:30:33 -0300

    Prática Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas

---
+++ 1-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/1/scripts-2.js
@@ -0,0 +1,25 @@
+let patientId = 50;
+
+let isEnrolled = true;
+
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId, typeof isEnrolled, typeof patientInfo, typeof patientEmail, typeof patientAge);
+
+
+const base = 5;
+
+let height = 8;
+
+const area = base * height;
+
+console.log(area);
+
+const perimeter = 2 * base + 2 * height;
+
+console .log(perimeter);
