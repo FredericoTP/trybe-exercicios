29ad6622086a4c7ee7b049763d6143d70b80a657
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-08-18 19:09:36 -0300

    exercício 5

---
+++ 1-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/1/exercicio-5.js
@@ -0,0 +1,13 @@
+let a = 165
+
+let b = 15
+
+let c = 120
+
+if (a + b + c == 180) {
+    console.log("true");
+} else if (a < 0 || b < 0 || c < 0) {
+    console.log("erro");
+} else {
+    console.log("false");
+}
