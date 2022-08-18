e6c72d30a2b227698757cbd1e57ea11341203480
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-08-18 20:32:19 -0300

    Exercício 7

---
+++ 1-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/1/exercicio-7.js
@@ -0,0 +1,17 @@
+let nota = 61
+
+if (nota >= 90 && nota <= 100) {
+    console.log("Nota A");
+} else if (nota >= 80 && nota < 90) {
+    console.log("Nota B");
+} else if (nota >= 70 && nota < 80) {
+    console.log("Nota C");
+} else if (nota >= 60 && nota < 70) {
+    console.log("Nota D");
+} else if (nota >= 50 && nota < 60) {
+    console.log("Nota E");
+} else if (nota >= 0 && nota < 50) {
+    console.log("Nota F");
+} else {
+    console.log("erro");
+}
