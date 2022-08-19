a5fb1a73f606b903eb64ae60ff31a3ec96bcee33
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-08-19 18:23:35 -0300

    Resolução exercícios 3 e 4

--- 1-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/2/scripts-2.js
+++ 1-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/2/scripts-2.js
@@ -17,3 +17,33 @@ for (let index = 0; index < numbers.length; index += 1) {
 }
 
 console.log(somaNumeros);
+
+// Exercício 3
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let somaNumeros = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    somaNumeros = somaNumeros + numbers[index];
+}
+
+console.log(somaNumeros / numbers.length);
+
+// Exercício 4
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let somaNumeros = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    somaNumeros = somaNumeros + numbers[index];
+}
+
+let media = somaNumeros / numbers.length;
+
+if (media > 20) {
+    console.log("valor maior que 20");
+} else {
+    console.log("valor menor ou igual a 20");
+}
