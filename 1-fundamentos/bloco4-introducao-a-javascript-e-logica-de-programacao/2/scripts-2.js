4ffa214fd0b37ad278c1ba5be0734cfeb0eef24b
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-08-19 21:56:53 -0300

    Resolução exercício bônus 3

--- 1-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/2/scripts-2.js
+++ 1-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/2/scripts-2.js
@@ -154,3 +154,19 @@ for (let index = 1; index < numbers.length; index += 1) {
 }
 
 console.log(numbers);
+
+//Bônus - Exercício 3
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let multiplicado = [];
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (index < numbers.length -1) {
+        multiplicado.push(numbers[index] * numbers[index + 1]);
+    } else {
+        multiplicado.push(numbers[index] * 2);
+    }
+}
+
+console.log(multiplicado);
