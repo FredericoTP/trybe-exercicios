8a83e870364c8f9d048f2df0b05bdeb319b86ae0
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-08-19 21:23:01 -0300

    Resolução exercícios bônus

--- 1-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/2/scripts-2.js
+++ 1-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/2/scripts-2.js
@@ -106,3 +106,51 @@ for (let index = 0; index < 25; index +=1) {
 }
 
 console.log(numeros);
+
+// Exercício 9
+
+let numeros = [];
+
+for (let index = 0; index < 25; index += 1) {
+    numeros.push(index + 1)
+}
+
+let numerosDivididos = [];
+
+for (let index2 = 0; index2 < numeros.length; index2 += 1) {
+    numerosDivididos.push(numeros[index2] / 2);
+}
+
+console.log(numerosDivididos);
+
+// Bônus - Exercício 1
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 1; index < numbers.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (numbers[index] < numbers[secondIndex]) {
+        let position = numbers[index];
+        numbers[index] = numbers[secondIndex];
+        numbers[secondIndex] = position;
+      }
+    }
+}
+
+console.log(numbers);
+
+// Bônus - Exercício 2
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 1; index < numbers.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (numbers[index] > numbers[secondIndex]) {
+        let position = numbers[index];
+        numbers[index] = numbers[secondIndex];
+        numbers[secondIndex] = position;
+      }
+    }
+}
+
+console.log(numbers);
