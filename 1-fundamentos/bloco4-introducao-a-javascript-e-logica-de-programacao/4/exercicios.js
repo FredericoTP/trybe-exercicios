d04c60afc6c4854b06bea04595f2e16df7f88762
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-08-23 15:42:15 -0300

    Resolução exercícios 5, 6 ,7 e 8

--- 1-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/4/exercicios.js
+++ 1-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/4/exercicios.js
@@ -50,3 +50,90 @@ for (let index in info) {
     console.log(info[index]);
 }
 
+// Exercício 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+info["recorrente"] = "Sim";
+
+let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim",
+};
+
+for (let index in info, info2) {
+    if (info[index] === info2[index]) {
+        console.log("Ambos " + index + "s");
+    } else {
+        console.log(info[index] + " e " + info2[index])
+    };
+}
+
+// Exercício 6 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+};
+
+console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + "'" + leitor.livrosFavoritos[0].titulo + "'");
+
+// Exercício 7 - Adicione um novo livro favorito na chave livrosFavoritos, que é um array.
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+};
+
+leitor.livrosFavoritos[1] = {
+    titulo: "Harry Potter e o Prisioneiro de Azkaban",
+    autor: "JK Rowling",
+    editora: "Rocco",
+};
+
+console.log(leitor);
+
+// Exercício 8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+};
+
+leitor.livrosFavoritos[1] = {
+    titulo: "Harry Potter e o Prisioneiro de Azkaban",
+    autor: "JK Rowling",
+    editora: "Rocco",
+};
+
+console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos");
