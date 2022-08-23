5e29a33b3a3bc802f93b9aa6b63a3ca7ab8d4deb
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-08-23 15:11:24 -0300

    Resolução exercícios 2, 3 e 4

--- 1-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/4/exercicios.js
+++ 1-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/4/exercicios.js
@@ -6,6 +6,47 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+};
 
-  console.log("Bem-vinda, " + info.personagem);
+console.log("Bem-vinda, " + info.personagem);
+
+// Exercício 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+info["recorrente"] = "Sim";
+
+console.log(info);
+
+// Exercício 3 - Faça um for/in que mostre todas as chaves do objeto.
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+info["recorrente"] = "Sim";
+
+for (let index in info) {
+    console.log(index);
+}
+
+// Exercício 4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto.
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+info["recorrente"] = "Sim";
+
+for (let index in info) {
+    console.log(info[index]);
+}
+
