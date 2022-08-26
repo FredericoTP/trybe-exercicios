6034de031b1dccb36278da2fd3f6fcce24422ea8
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-08-26 16:04:16 -0300

    Resolução exercícios 4, 5 e 6

--- 1-fundamentos/bloco5-javascript-dom-eventos-e-web-storage/2/script-2.js
+++ 1-fundamentos/bloco5-javascript-dom-eventos-e-web-storage/2/script-2.js
@@ -28,3 +28,27 @@ main[0].appendChild(createSectionMain);
 
 // Exercício 4 - Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
 
+let createP = document.createElement("p");
+
+main[0].children[0].appendChild(createP);
+
+let pMain = document.querySelector(".main-content .center-content p");
+
+pMain.innerText = "Aprendendo a criar o HTML utilizando o JavaScript";
+
+// Exercício 5 - Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+
+let createSection2Main = document.createElement("section");
+
+createSection2Main.className = "left-content";
+
+main[0].appendChild(createSection2Main);
+
+// Exercício 6 - Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+
+let createSection3Main = document.createElement("section");
+
+createSection3Main.className = "right-content";
+
+main[0].appendChild(createSection3Main);
+
