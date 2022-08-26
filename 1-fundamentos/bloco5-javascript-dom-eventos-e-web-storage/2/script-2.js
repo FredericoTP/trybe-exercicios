a193b160dd3a30f1d779cbdae2d2d70b78f73aa1
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-08-26 17:51:48 -0300

    Resolução exercícios 7, 8 e 9

--- 1-fundamentos/bloco5-javascript-dom-eventos-e-web-storage/2/script-2.js
+++ 1-fundamentos/bloco5-javascript-dom-eventos-e-web-storage/2/script-2.js
@@ -52,3 +52,43 @@ createSection3Main.className = "right-content";
 
 main[0].appendChild(createSection3Main);
 
+// Exercício 7 - Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image. Esse elemento deve ser filho do section criado no passo 5; 
+
+let sectionLeft = document.getElementsByClassName("left-content");
+
+let createImage = document.createElement("img");
+
+createImage.className = "small-image";
+
+createImage.src = "https://picsum.photos/200";
+
+sectionLeft[0].appendChild(createImage);
+
+// Exercício 8 - Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um, dois, três, ... como valores da lista. Essa lista deve ser filha do section criado no passo 6;
+
+let list = ["Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez"];
+
+let sectionRight = document.getElementsByClassName("right-content");
+
+let createUl = document.createElement("ul");
+
+sectionRight[0].appendChild(createUl);
+
+let ulRight = document.getElementsByTagName("ul");
+
+for (let index = 0; index < list.length; index += 1) {
+    let createLi = document.createElement("li");
+    createLi.innerText = list[index];
+    ulRight[0].appendChild(createLi);
+}
+
+// exercício 9 - Adicione 3 tags h3, todas sendo filhas do main criado no passo 2.
+
+function createElementInMain(tag) {
+    let createH3 = document.createElement(tag);
+    main[0].appendChild(createH3);
+}
+
+createElementInMain("h3");
+createElementInMain("h3");
+createElementInMain("h3");
