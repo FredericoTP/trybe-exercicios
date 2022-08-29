81dc1b07cc31d24dae927684b329093284e31a55
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-08-29 19:46:59 -0300

    Resolução exercício 8

--- 1-fundamentos/bloco5-javascript-dom-eventos-e-web-storage/3/script.js
+++ 1-fundamentos/bloco5-javascript-dom-eventos-e-web-storage/3/script.js
@@ -147,3 +147,18 @@ function createTask(string) {
 }
 
 createTask("Cozinhar");
+
+// Exercício 8 - Implemente uma função que adicione uma legenda com cor para a tarefa.
+//   Essa função deverá receber como parâmetro uma string ('cor') e criar dinamicamente um elemento de tag `<div>` com a classe `task`.
+//   O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+//   O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks". 
+
+function subtitleSpan(cor) {
+    const div = document.createElement("div");
+    div.className = "task";
+    const divTask = document.getElementsByClassName("my-tasks");
+    div.style.backgroundColor = cor;
+    divTask[0].appendChild(div);
+}
+
+subtitleSpan("red");
