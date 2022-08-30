c487705a9ac76372673d697062a74629293bd77e
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-08-30 15:35:20 -0300

    Adição de botões

--- 1-fundamentos/bloco5-javascript-dom-eventos-e-web-storage/4/script-3.js
+++ 1-fundamentos/bloco5-javascript-dom-eventos-e-web-storage/4/script-3.js
@@ -4,3 +4,30 @@ const textColor = document.getElementById("text-color");
 const fontSize = document.getElementById("font-size");
 const lineHeight = document.getElementById("line-height");
 const fontName = document.getElementById("font-name");
+
+let listBgColors = ["White","Black", "Silver", "SkyBlue", "DarkSlateGray", "NavajoWhite"];
+
+let listTextColors = ["White", "Black", "Red", "Indigo", "SkyBlue", "Honeydew"];
+
+let listFontSize = ["5px", "10px", "25px", "20px", "25px", "30px"];
+
+let listLineHeight = ["3px", "6px", "9px", "12px", "15px", "18px"];
+
+let listFontName = ["Arial", "Monaco", "Copperplate", "Times New Roman", "Lucida Handwriting"];
+
+
+function createButton(list, element) {
+    
+    for (let index = 0; index < list.length; index += 1) {
+        let listItem = list[index];
+        let button = document.createElement("button");
+        button.innerText = listItem;
+        element.appendChild(button);
+    }
+}
+
+createButton(listBgColors, bgColor);
+createButton(listTextColors, textColor);
+createButton(listFontSize, fontSize);
+createButton(listLineHeight, lineHeight);
+createButton(listFontName, fontName);
