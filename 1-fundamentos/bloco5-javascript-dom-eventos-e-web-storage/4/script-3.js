c07b686f73b1e43f21db36224d5fd35e7942f222
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-08-30 15:06:40 -0300

    Criação do esqueleto da página

---
+++ 1-fundamentos/bloco5-javascript-dom-eventos-e-web-storage/4/script-3.js
@@ -0,0 +1,6 @@
+const preferences = document.getElementById("preferences");
+const bgColor = document.getElementById("background-color");
+const textColor = document.getElementById("text-color");
+const fontSize = document.getElementById("font-size");
+const lineHeight = document.getElementById("line-height");
+const fontName = document.getElementById("font-name");
