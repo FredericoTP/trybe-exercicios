ca0114c13a9b4811069092c5b36bcb29b6d6806b
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-06 15:20:00 -0300

    Bônus 1 e 2

--- 1-fundamentos/bloco6-html-e-css-forms-flexbox-e-responsivo/1/script-form.js
+++ 1-fundamentos/bloco6-html-e-css-forms-flexbox-e-responsivo/1/script-form.js
@@ -14,9 +14,25 @@
 
 // 4 - Crie um botão que limpe as informações contidas nos campos.
 
+// Bônus 1 - A TrybeTrip precisa muito de fotos para divulgar seus concursos. Tendo isso em mente, faça com que somente quem autorizar o uso de imagens possa enviar suas informações. 
 
-const buttonSubmit = document.getElementById('button-submit')
+// Bônus 2 - Faça a validação dos campos com limite de caracteres. Caso não estejam dentro do esperado ao clicar no botão de submit, um alerta deve ser mostrado com a mensagem: 'Dados Inválidos'. Caso contrário, a mensagem 'Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.' deverá aparecer na tela. 
+
+const buttonSubmit = document.getElementById('button-submit');
+const checkboxDireitoImagem = document.getElementById('checkbox2');
+const nome = document.getElementById('name');
+const email = document.getElementById('email');
+const textMotivo = document.getElementById('text-motivo');
 
 buttonSubmit.addEventListener('click', function(event) {
-    event.preventDefault();
+    if (checkboxDireitoImagem.checked === false) {
+        alert('Concorde com o uso de imagens!')
+        event.preventDefault();
+    } else if (nome.value.length < 10 || email.value.length < 10 || textMotivo.value.length == 0) {
+        alert('Dados Inválidos!');
+        event.preventDefault();
+    } else {
+        alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.')
+    }
 })
+
