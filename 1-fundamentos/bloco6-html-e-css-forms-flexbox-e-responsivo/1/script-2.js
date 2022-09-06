81544a49650d6ac11f739bbc353dc878927b8e4b
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-06 12:50:14 -0300

    Practice 1

--- 1-fundamentos/bloco6-html-e-css-forms-flexbox-e-responsivo/1/script-2.js
+++ 1-fundamentos/bloco6-html-e-css-forms-flexbox-e-responsivo/1/script-2.js
@@ -1,3 +1,9 @@
 const INPUT_TEXT = document.querySelector("#input-text");
 const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
 const HREF_LINK = document.querySelector("#href");
+
+// 1 - Adicione um evento click no elemento a que possui o id href. Na função do evento, implemente o método .preventDefault() para previnir o comportamento padrão do elemento. Ou seja, ao clicar no link, nada deve acontecer.
+
+HREF_LINK.addEventListener('click', function(event) {
+    event.preventDefault();
+})
