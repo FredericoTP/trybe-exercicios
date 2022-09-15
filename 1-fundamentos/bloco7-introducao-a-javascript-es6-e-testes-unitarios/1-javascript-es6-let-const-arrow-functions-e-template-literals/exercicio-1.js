7742d9bc4dfc8d4d7f66a31091f344dfa0f82562
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-15 14:45:23 -0300

    Resolução exercício 2

--- 1-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio-1.js
+++ 1-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio-1.js
@@ -4,15 +4,42 @@
 // Modifique as concatenações para template literals. 
 
 const testingScope = escopo => {
-    if (escopo === true) {
-        let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-        ifScope = `${ifScope} ótimo, fui utilizada no escopo!`;
-        console.log(ifScope);
-    } else {
-        const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-        console.log(elseScope);
-    }
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo!`;
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
 }
 
 testingScope(true);
 testingScope(false);
+
+// 2- Crie uma função que retorne um array em ordem crescente.
+// Crie uma função que faça com que o array oddsAndEvens fique em ordem crescente; 
+// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne a frase "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!". 
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort(). Spoiler: É possível realizar uma função que ordene qualquer array de números. 
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = () => {
+  for (let index = 1; index < oddsAndEvens.length; index += 1) {
+    for (let index2 = 0; index2 < index; index2 += 1) {
+      if (oddsAndEvens[index] < oddsAndEvens[index2]) {
+        let positon = oddsAndEvens[index];
+        oddsAndEvens[index] = oddsAndEvens[index2]
+        oddsAndEvens[index2] = positon;
+      }
+    }
+  }
+  return oddsAndEvens;
+}
+
+console.log(sortOddsAndEvens());
+
+
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+// oddsAndEvens.sort((a, b) => a - b);
+// console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
