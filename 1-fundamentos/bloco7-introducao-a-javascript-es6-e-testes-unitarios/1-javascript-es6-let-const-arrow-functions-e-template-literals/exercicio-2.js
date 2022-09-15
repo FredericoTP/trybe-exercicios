4e737547619edb91fdc512c3ce04a79727c57412
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-15 15:46:29 -0300

    Resolução exercício 2

--- 1-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio-2.js
+++ 1-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio-2.js
@@ -19,3 +19,30 @@ console.log(factorial(5));
 const factorial = number => number > 1 ? number * factorial(number - 1) : 1;
 
 console.log(`Esse é o fatorial ${factorial(5)}`);
+
+// 2- Crie uma função que receba uma frase como parâmetro e retorne a maior palavra contida nesta frase.
+
+const maiorPalavra = frase => {
+  const array = frase.split(' ');
+  let contador = 0;
+  let palavra;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index].length > contador) {
+      contador = array[index].length;
+      palavra = array[index];
+    }
+  }
+  return palavra;
+}
+
+console.log(maiorPalavra('Antônio foi ao banheiro e não sabemos o que aconteceu'));
+
+const maiorPalavra = frase => {
+  let array = frase.split(' ');
+  array.sort((a, b) => a - b);
+
+  return array[array.length - 1];
+}
+
+console.log(maiorPalavra('Antônio foi ao banheiro e não sabemos o que aconteceu'));
+
