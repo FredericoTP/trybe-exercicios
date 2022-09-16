95131e67352f0bfd3667040b215adbfe4f731ce6
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-16 13:03:08 -0300

    Resolução Exercício throw try/catch

--- 1-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/2-javascript-es6-fluxo-de-exceção-e-objetos/script.js
+++ 1-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/2-javascript-es6-fluxo-de-exceção-e-objetos/script.js
@@ -10,12 +10,31 @@
 function sum() {
   const value1 = document.getElementById('value1').value;
   const value2 = document.getElementById('value2').value;
+
+if (value1 === '' || value2 === '') {
+  throw new Error('Erro! Insira os valores para somar!');
+} else if (isNaN(value1) || isNaN(value2)) {
+  throw new Error('Error! Os caracteres não são um número!')
+}
+
   const result = Number(value1) + Number(value2);
   document.getElementById('result').innerHTML = `Resultado: ${result}`;
   document.getElementById('value1').value = '';
   document.getElementById('value2').value = '';
 }
+
+const verificacao = () => {
+  try {
+    sum();
+  } catch (error) {
+    document.getElementById('result').innerHTML = error.message;
+  } finally {
+    document.getElementById('value1').value = '';
+    document.getElementById('value2').value = '';
+  }
+}
+
 window.onload = () => {
   const button = document.getElementById('button');
-  button.addEventListener('click', sum);
+  button.addEventListener('click', verificacao);
 }
