c82937394bbcea2fdf0826f587cfaead757629fe
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-16 13:35:28 -0300

    Adição de  arquivo para Exercícios - Parte III

---
+++ 1-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/2-javascript-es6-fluxo-de-exceção-e-objetos/script-parte3.js
@@ -0,0 +1,23 @@
+// Parte III
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// Crie uma função para adicionar o turno da noite na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela. 
