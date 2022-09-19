c29f9ed7eebd59f0e2872b8d0472febce413df8b
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-19 15:14:48 -0300

    Adição de arquivos para exercício 2

---
+++ 1-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/3-primeiros-passos-em-jest/myRemove.test.js
@@ -0,0 +1,5 @@
+// A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array 
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado 
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4] 
+// Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado 
+
