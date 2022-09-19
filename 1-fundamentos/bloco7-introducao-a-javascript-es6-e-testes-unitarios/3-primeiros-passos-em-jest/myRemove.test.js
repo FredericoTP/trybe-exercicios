30eb856d05104ca7f8171339be3afd8ed01ee08c
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-19 15:39:42 -0300

    Resolução exercício 2

--- 1-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/3-primeiros-passos-em-jest/myRemove.test.js
+++ 1-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/3-primeiros-passos-em-jest/myRemove.test.js
@@ -3,3 +3,16 @@
 // Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4] 
 // Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado 
 
+const myRemove = require('./myRemove');
+
+describe('Teste myRemove', () => {
+  test('myRemove retorna [1, 2, 4] se myRemove([1, 2, 3, 4], 3)', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+  test('myRemove retorna [1, 2, 4] se myRemove([1, 2, 3, 4], 3)', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+  test('myRemove retorna [1, 2, 4] se myRemove([1, 2, 3, 4], 3)', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
