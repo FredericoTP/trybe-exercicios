8d4e379568e87e6b9b508fef84fc63269b9450ff
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-22 12:05:23 -0300

    Resolução exercício 'Sorteador de loteria'

--- 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/1-javascript-es6-introducao-a-higher-order-functions/exercise.js
+++ 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/1-javascript-es6-introducao-a-higher-order-functions/exercise.js
@@ -17,3 +17,22 @@ const newEmployees = (func) => {
 };
 
 console.log(newEmployees(pessoaContratada));
+
+// 2- Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: “Tente novamente” ou “Parabéns você ganhou”).
+
+const numberCheck = (number1, number2) => {
+  if (number1 === number2) {
+    return 'Parabéns você ganhou!';
+  }
+
+  return 'Tente novamente!';
+}
+
+const lotery = (number, func) => {
+  const numberRandom = Math.floor(Math.random() * 5 + 1);
+  const check = func(number, numberRandom);
+
+  return check;
+};
+
+console.log(lotery(3, numberCheck));
