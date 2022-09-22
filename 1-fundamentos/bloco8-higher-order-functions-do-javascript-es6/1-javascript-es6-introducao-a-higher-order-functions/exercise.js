062a123c1071c492c08d172262ca539076dad965
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-22 12:52:07 -0300

    Resolução exercício 'Corretor automático de exame'

--- 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/1-javascript-es6-introducao-a-higher-order-functions/exercise.js
+++ 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/1-javascript-es6-introducao-a-higher-order-functions/exercise.js
@@ -36,3 +36,33 @@ const lotery = (number, func) => {
 };
 
 console.log(lotery(3, numberCheck));
+
+// 3- Crie uma HOF que receberá três parâmetros:
+// O primeiro será um array de respostas corretas (soluções); 
+// O segundo será um array contendo as respostas fornecidas por uma pessoa estudante; 
+// O terceiro é uma função que compara os dois arrays e então dá uma pontuação baseada nos acertos. Para isso, essa função usará os seguintes critérios: 
+// Uma resposta correta adiciona 1 ponto à pontuação final; 
+// A ausência de uma resposta não altera a pontuação (quando for “N.A”); 
+// Uma resposta incorreta reduz a pontuação final em 0.5 ponto. 
+// Ao final, a HOF deve retornar o total de pontos obtidos através das respostas fornecidas pela pessoa estudante.
+
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const compareAnswers = (array1, array2) => {
+  let result = 0;
+
+  for (let index = 0; index < array1.length; index += 1) {
+    if (array1[index] === array2[index]) {
+      result += 1;
+    } else if (array1[index] !== array2[index] && array2[index] !== 'N.A') {
+      result -= 0.5;
+    }
+  }
+
+  return result;
+};
+
+const points = (answers1, answers2, func) => func(answers1, answers2);
+
+console.log(points(rightAnswers, studentAnswers, compareAnswers));
