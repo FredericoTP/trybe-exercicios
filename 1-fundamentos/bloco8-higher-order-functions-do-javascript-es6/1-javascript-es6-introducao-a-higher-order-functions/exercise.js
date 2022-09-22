4f09100669357d0b5331cdc10d337aaa50182434
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-22 11:53:40 -0300

    Resolução exercício 'Nova pessoa contratada'

--- 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/1-javascript-es6-introducao-a-higher-order-functions/exercise.js
+++ 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/1-javascript-es6-introducao-a-higher-order-functions/exercise.js
@@ -1,2 +1,19 @@
 // 1- Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id. A sua função deve receber como parâmetro o nome completo da pessoa funcionária e, a partir dele, gerar automaticamente um email no formato nome_da_pessoa@trybe.com.
 
+const pessoaContratada = (nome) => {
+  const nomeSplit = nome.split(' ');
+  const emailPessoa = nomeSplit.join('_');
+  return { nomeCompleto: nome, email: `${emailPessoa.toLowerCase()}@trybe.com`};
+};
+
+const newEmployees = (func) => {
+  const employees = {
+    id1: func('Pedro Guerra'),
+    id2: func('Luiza Drumond'),
+    id3: func('Carla Paiva'),
+  }
+
+  return employees;
+};
+
+console.log(newEmployees(pessoaContratada));
