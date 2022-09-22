febb9cadcfbddd73910fecbfbc8e7bd11ba823a3
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-22 13:16:09 -0300

    Resolução exercício bônus - parte I

--- 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/1-javascript-es6-introducao-a-higher-order-functions/exercise-bonus.js
+++ 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/1-javascript-es6-introducao-a-higher-order-functions/exercise-bonus.js
@@ -1,24 +1,54 @@
 const mage = {
-    healthPoints: 130,
-    intelligence: 45,
-    mana: 125,
-    damage: undefined,
-  };
-  
-  const warrior = {
-    healthPoints: 200,
-    strength: 30,
-    weaponDmg: 2,
-    damage: undefined,
-  };
-  
-  const dragon = {
-    healthPoints: 350,
-    strength: 50,
-    damage: undefined,
-  };
-  
-  const battleMembers = { mage, warrior, dragon };
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
 
-  // Parte I
-  // Crie uma função que retorna o dano do dragão. O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo). 
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+// Parte I
+// Crie uma função que retorna o dano do dragão. O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo). 
+
+const dragonDamage = () => {
+  const dragonStrength = dragon.strength;
+
+  return Math.floor(Math.random() * (dragonStrength - 15)) + 15;
+};
+
+// Crie uma função que retorna o dano causado pelo warrior. O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo). 
+
+const warriorDamage = () => {
+  const warriorStrength = warrior.strength;
+  const warriorWeaponDmg = warrior.weaponDmg;
+
+  return Math.floor(Math.random() * (warriorStrength * warriorWeaponDmg - warriorStrength)) + warriorStrength;
+};
+
+// Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno. 
+// O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo). 
+// A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: “Não possui mana suficiente”) e a mana gasta é 0. 
+
+const mageDamage = () => {
+  const mageIntelligence = mage.intelligence;
+  const mageMana = mage.mana;
+
+  if (mageMana >= 15) {
+    return { damage: Math.floor(Math.random() * (mageIntelligence * 2 - mageIntelligence)) + mageIntelligence, mana: mageMana - 15 };
+  }
+
+  return { damage: 0, mana: 'Não possui mana suficiente!' };
+};
