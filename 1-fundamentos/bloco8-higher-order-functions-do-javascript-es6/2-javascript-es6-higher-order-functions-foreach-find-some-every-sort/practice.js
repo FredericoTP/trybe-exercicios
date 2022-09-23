68049f1a12f1d3d50e80d12977638f1986abc8c6
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-23 13:14:43 -0300

    Resolução practice every

--- 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/practice.js
+++ 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/practice.js
@@ -67,3 +67,21 @@ const hasName = (arr, name) => {
 
 console.log(hasName(names2, 'Ana'));
 
+// every
+
+// Escreva uma função, que dado um array de pessoas e uma idade mínima, retorne true se todas tiverem a idade maior ou igual à mínima e, caso contrário, false.
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  return arr.every((element) => element.age >= minimumAge);
+};
+
+console.log(verifyAges(people, 18));
+
