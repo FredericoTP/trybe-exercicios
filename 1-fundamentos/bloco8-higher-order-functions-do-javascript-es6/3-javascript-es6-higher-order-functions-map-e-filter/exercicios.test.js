38b3d3278282f6fa9726dde5963439ecf091c3a3
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-26 15:31:42 -0300

    Adição de testes para exercícios 3 e 4

--- 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/3-javascript-es6-higher-order-functions-map-e-filter/exercicios.test.js
+++ 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/3-javascript-es6-higher-order-functions-map-e-filter/exercicios.test.js
@@ -17,3 +17,21 @@ describe('testando function nameAndAge', () => {
     expect(nameAndAge()).toEqual([{ author: 'Isaac Asimov', age: 31 }, { author: 'H. P. Lovecraft', age: 38 }, { author: 'Stephen King', age: 39 }, { author: 'George R. R. Martin', age: 43 }, { author: 'Frank Herbert', age: 45 }, { author: 'J. R. R. Tolkien', age: 62 }]);
   });
 });
+
+describe('testando function fantasyOrScienceFiction', () => {
+  it('is function', () => {
+    expect(typeof fantasyOrScienceFiction).toBe('function');
+  });
+  it('returns the right answer when called', () => {
+    expect(fantasyOrScienceFiction()).toEqual([{id: 1, name: 'As Crônicas de Gelo e Fogo', genre: 'Fantasia', author: { name: 'George R. R. Martin', birthYear: 1948 }, releaseYear: 1991}, {id: 2, name: 'O Senhor dos Anéis', genre: 'Fantasia', author: { name: 'J. R. R. Tolkien', birthYear: 1892 }, releaseYear: 1954}, {id: 3, name: 'Fundação', genre: 'Ficção Científica', author: { name: 'Isaac Asimov', birthYear: 1920 }, releaseYear: 1951}, {id: 4, name: 'Duna', genre: 'Ficção Científica', author: { name: 'Frank Herbert', birthYear: 1920 }, releaseYear: 1965}]);
+  });
+});
+
+describe('testando function oldBooksOrdered', () => {
+  it('is function', () => {
+    expect(typeof oldBooksOrdered).toBe('function');
+  });
+  it('returns the right answer when called', () => {
+    expect(oldBooksOrdered()).toEqual([{id: 6, name: 'O Chamado de Cthulhu', genre: 'Terror', author: { name: 'H. P. Lovecraft', birthYear: 1890 }, releaseYear: 1928}, {id: 3, name: 'Fundação', genre: 'Ficção Científica', author: { name: 'Isaac Asimov', birthYear: 1920 }, releaseYear: 1951}, {id: 2, name: 'O Senhor dos Anéis', genre: 'Fantasia', author: { name: 'J. R. R. Tolkien', birthYear: 1892 }, releaseYear: 1954}]);
+  });
+});
