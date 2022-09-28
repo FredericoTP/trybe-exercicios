5922748ab2105b8b2f807f43bdadcddeeea95096
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-28 13:39:41 -0300

    Resolução exercício 6

--- 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise-6.js
+++ 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise-6.js
@@ -4,3 +4,8 @@ const palio = ['Palio', 'Fiat', 2019];
 const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
+const toObject = ([modelo, fabricante, ano]) => ({modelo, fabricante, ano});
+
+console.log(toObject(palio));
+console.log(toObject(shelbyCobra));
+console.log(toObject(chiron));
