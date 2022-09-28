f806fa8474c69420b8dc13710c00dfc367638b02
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-28 11:41:17 -0300

    Resolução practice Array Destructuring

--- 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/practice.js
+++ 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/practice.js
@@ -38,3 +38,31 @@ const apresenation = `Hi, my name is ${name}, I'm ${age} years old and I'm ${nat
 
 // console.log(apresenation);
 
+// 3-  Array Destructuring
+
+// Produza o mesmo resultado do código, porém utilizando o array destructuring para recolher a função e a saudação. 
+
+const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+const [ frase, funcao ] = saudacoes;
+
+// funcao(frase);
+
+// A seguir, temos alguns valores que estão descritos em variáveis incoerentes. Através da desestruturação de arrays, corrija os valores das variáveis. 
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+[comida, animal, bebida] = [bebida, comida, animal];
+
+// console.log(comida, animal, bebida);
+
+// O array abaixo possui alguns números que não condizem com o conteúdo que ele deveria possuir. Através de array destructuring, faça com que existam apenas números pares na variável numerosPares. 
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+[,,, ...numerosPares] = numerosPares;
+
+// console.log(numerosPares);
+
