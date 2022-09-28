1a4f7e94d8dbb6f40039c7ce26cf8496c11c31d9
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-28 13:10:56 -0300

    Resolução exercício 3

--- 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise-3.js
+++ 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise-3.js
@@ -13,13 +13,8 @@ const gunnar = {
   likes: ['hiking', 'scuba diving', 'taking pictures'],
   nationality: 'Icelandic',
 };
-// name: nome da pessoa, age: idade, likes: gosta de, nationality: nacionalidade
 
-// complete a assinatura da função abaixo
-const personLikes = () => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
-// <nome> tem <anos de idade> e gosta de <gostos da pessoa>
+const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
 
-// Retornos esperados:
-console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
-console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
-
+console.log(personLikes(alex));
+console.log(personLikes(gunnar));
