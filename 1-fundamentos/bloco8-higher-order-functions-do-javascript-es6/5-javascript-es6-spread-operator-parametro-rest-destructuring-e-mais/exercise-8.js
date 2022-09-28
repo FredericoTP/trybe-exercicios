b9d32f352066ccf374e6ad4fba4f6ed57d18f50d
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-28 15:42:06 -0300

    Resolução exercício 8

--- 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise-8.js
+++ 1-fundamentos/bloco8-higher-order-functions-do-javascript-es6/5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise-8.js
@@ -1,8 +1,7 @@
 // Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento. Um parâmetro adicional pode ser passado para alterar o cumprimento utilizado.
 
-// escreva greet abaixo
+const greet = (name, greeting = 'Hi') => `${greeting} ${name}`;
 
-// Retornos esperados:
-console.log(greet('John')); // 'Hi John'
-console.log(greet('John', 'Good morning')); // 'Good morning John'
-console.log(greet('Isabela', 'Oi')); // 'Oi Isabela'
+console.log(greet('John'));
+console.log(greet('John', 'Good morning'));
+console.log(greet('Isabela', 'Oi'));
