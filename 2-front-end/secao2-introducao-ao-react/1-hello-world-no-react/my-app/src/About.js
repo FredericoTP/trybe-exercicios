44d68edaa18aff79e1e6f1198230dd97defa7b20
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-10-19 15:16:37 -0300

    Estilização

--- 2-front-end/secao2-introducao-ao-react/1-hello-world-no-react/my-app/src/About.js
+++ 2-front-end/secao2-introducao-ao-react/1-hello-world-no-react/my-app/src/About.js
@@ -4,10 +4,10 @@ class About extends React.Component {
   render() {
     return (
       <div>
-        <h1>Frederico Tavares Pessoa</h1>
-        <p>Gosto muito de jogos, tecnologia, engenharia, fotografia, programação e astronomia. No momento estou migrando de área como estudante de Desenvolvimento Web Full Stack na Trybe.</p>
-        <h2>Minhas Habilidades</h2>
-        <ul>
+        <h1 className="title">Frederico Tavares Pessoa</h1>
+        <p className="text">Gosto muito de jogos, tecnologia, engenharia, fotografia, programação e astronomia. No momento estou migrando de área como estudante de Desenvolvimento Web Full Stack na Trybe.</p>
+        <h2 className="title">Minhas Habilidades</h2>
+        <ul className="text">
           <li>Unix & Bash</li>
           <li>HTML</li>
           <li>CSS</li>
--- 2-front-end/secao2-introducao-ao-react/1-hello-world-no-react/my-app/src/App.js
+++ 2-front-end/secao2-introducao-ao-react/1-hello-world-no-react/my-app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import About from './About';
 
 class App extends React.Component {
