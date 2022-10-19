28093cb6df1b1cb504a74af55e82fbbcb4a0f359
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-10-19 15:04:40 -0300

    Adição de informações em AboutMe

---
+++ 2-front-end/secao2-introducao-ao-react/1-hello-world-no-react/my-app/src/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class AboutMe extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Frederico Tavares Pessoa</h1>
+        <p>Gosto muito de jogos, tecnologia, engenharia, fotografia, programação e astronomia. No momento estou migrando de área como estudante de Desenvolvimento Web Full Stack na Trybe.</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>Unix & Bash</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default AboutMe;
