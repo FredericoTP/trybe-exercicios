b784bcd4290c3b27dd583365db5799597a8cfbd7
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-10-20 13:40:50 -0300

    Resolução exercícios de fixação - Props

--- 2-front-end/secao2-introducao-ao-react/2-componentes-react/exercise-image-component/src/App.js
+++ 2-front-end/secao2-introducao-ao-react/2-componentes-react/exercise-image-component/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Image from './Image';
+import cat from './cat.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Image source={cat} alternativeText={'Cute cat staring'}/>
+    );
+  }
 }
 
 export default App;
