1b4ea6157b75df7a747d5308bb849bc1d454406c
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-10-25 14:30:14 -0300

    Practice 3 - validação FavoriteFood

--- 2-front-end/secao3-componentes-com-estado-eventos-e-formulários-com-react/2-formularios-no-react/my-form-1.0/src/components/FavoriteFood.jsx
+++ 2-front-end/secao3-componentes-com-estado-eventos-e-formulários-com-react/2-formularios-no-react/my-form-1.0/src/components/FavoriteFood.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 class FavoriteFood extends React.Component {
   render() {
     const { value, handleChange } = this.props;
+
+    let erro = undefined;
+    if (value.length > 25) {
+      erro = 'Texto muito grande';
+    }
     
     return (
       <label htmlFor="favoriteFood">
         Comida favorita:
         <textarea name="favoriteFood" id="favoriteFood" cols="20" rows="5" value={ value } onChange={ handleChange }></textarea>
+        <spam>{ erro }</spam>
       </label>
     );
   }
