593f77c2e08abcbb38b5bed90d89541f0101c265
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-10-25 13:42:24 -0300

    Practice 2

--- 2-front-end/secao3-componentes-com-estado-eventos-e-formulários-com-react/2-formularios-no-react/my-form-1.0/src/components/form.jsx
+++ 2-front-end/secao3-componentes-com-estado-eventos-e-formulários-com-react/2-formularios-no-react/my-form-1.0/src/components/form.jsx
@@ -6,43 +6,65 @@ class Form extends React.Component {
 
     this.state = {
       inputName: '',
+      inputEmail: '',
+      favoriteFood: '',
+      color: '',
+      inputCheckbox: false,
     }
 
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
+    const { target } = event;
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
     this.setState({
-      inputName: event.target.value,
+      [name]: value,
     })
   }
 
   render() {
     return (
       <form>
-        <label htmlFor="inputName">
-          Nome:
-          <input name="inputName" type="text" id="inputName" value={ this.state.inputName } onChange={ this.handleChange } />
-        </label>
+        <fieldset>
 
-        <label htmlFor="inputEmail">
-          Email:
-          <input type="text" id="inputEmail"/>
-        </label>
+          <label htmlFor="inputName">
+            Nome:
+            <input name="inputName" type="text" id="inputName" value={ this.state.inputName } onChange={ this.handleChange } />
+          </label>
 
-        <label htmlFor="favoriteFood">
-          Comida favorita:
-          <textarea name="favoriteFood" id="favoriteFood" cols="20" rows="5"></textarea>
-        </label>
+          <label htmlFor="inputEmail">
+            Email:
+            <input name="inputEmail" type="text" id="inputEmail"value={ this.state.inputEmail } onChange={ this.handleChange }/>
+          </label>
 
-        <label htmlFor="selectCor">
-          Escolha uma cor:
-          <select name="color" id="selectCor">
-            <option value="verde">Verde</option>
-            <option value="vermelho">Vermelho</option>
-            <option value="azul">Azul</option>
-          </select>
-        </label>
+          <label htmlFor="favoriteFood">
+            Comida favorita:
+            <textarea name="favoriteFood" id="favoriteFood" cols="20" rows="5" value={ this.state.favoriteFood } onChange={ this.handleChange }></textarea>
+          </label>
+
+          <label htmlFor="selectCor">
+            Escolha uma cor:
+            <select name="color" id="selectCor" value={ this.state.color } onChange={ this.handleChange }>
+              <option value="verde">Verde</option>
+              <option value="vermelho">Vermelho</option>
+              <option value="azul">Azul</option>
+            </select>
+          </label>
+
+          <label htmlFor="inputCheckbox">
+            Você desenha?
+            <input name="inputCheckbox" type="checkbox" id="inputCheckbox" value={ this.state.inputCheckbox } onChange={ this.handleChange }/>
+          </label>
+
+          <label htmlFor="">
+            Escolha uma foto
+            <input type="file" />
+          </label>
+          
+        </fieldset>
       </form>
     );
   }
