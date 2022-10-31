c623de99f7db7ca0921e72bc41fa2825cab19542
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-10-31 13:58:54 -0300

    Criação da função fetch

--- 2-front-end/secao4-ciclo-de-vida-de-componentes-e-react-router/1-ciclo-de-vida-de-componentes/random-user-app/src/App.jsx
+++ 2-front-end/secao4-ciclo-de-vida-de-componentes-e-react-router/1-ciclo-de-vida-de-componentes/random-user-app/src/App.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      loading: true,
+      user: [],
+    }
+  }
+
+  async fetchRandomUser() {
+    const url = 'https://api.randomuser.me/';
+    const response = await fetch(url);
+    const data = response.json();
+
+    this.setState({
+      user: data.results,
+    });
+  }
+
   render() {
     return (
       'asd'
