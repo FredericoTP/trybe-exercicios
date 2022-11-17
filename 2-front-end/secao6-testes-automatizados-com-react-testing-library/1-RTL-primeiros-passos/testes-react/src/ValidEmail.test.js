a13089a7a79a84a07a3fe7316331d288bb2b1677
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-11-17 13:42:25 -0300

    Adiciona testes para verificar a cor do texto e se não for inserido email

--- 2-front-end/secao6-testes-automatizados-com-react-testing-library/1-RTL-primeiros-passos/testes-react/src/ValidEmail.test.js
+++ 2-front-end/secao6-testes-automatizados-com-react-testing-library/1-RTL-primeiros-passos/testes-react/src/ValidEmail.test.js
@@ -15,3 +15,26 @@ test('Testando um componente, caso o email seja inválido.', () => {
   const isValid = screen.getByText('Email Inválido');
   expect(isValid).toBeInTheDocument();
 });
+
+test('Testando um componente, caso não seja inserido email', () => {
+  const EMAIL_USER = '';
+  render(<ValidEmail email={ EMAIL_USER } />);
+  const isValid = screen.queryByText('Email Válido');
+  const isInvalid = screen.queryByText('Email Inválido');
+  expect(isValid).not.toBeInTheDocument();
+  expect(isInvalid).not.toBeInTheDocument();
+});
+
+test('Verifica se o texto tem a cor vermelha caso o email seja inválido', () => {
+  const EMAIL_USER = 'emailerrado';
+  render(<ValidEmail email={ EMAIL_USER } />);
+  const isValid = screen.getByText('Email Inválido');
+  expect(isValid).toHaveAttribute('style', 'color: red;');
+});
+
+test('Verifica se o texto tem a cor verde caso o email seja válido', () => {
+  const EMAIL_USER = 'test@test.com';
+  render(<ValidEmail email={ EMAIL_USER } />);
+  const isValid = screen.getByText('Email Válido');
+  expect(isValid).toHaveAttribute('style', 'color: green;');
+});
