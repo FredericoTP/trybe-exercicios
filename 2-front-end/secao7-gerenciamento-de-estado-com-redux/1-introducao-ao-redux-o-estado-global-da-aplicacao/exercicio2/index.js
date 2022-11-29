4684ba51946e754dd78a29284b8b79442d787149
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-11-29 14:50:49 -0300

    Adiciona arquivos para Exercício 2

---
+++ 2-front-end/secao7-gerenciamento-de-estado-com-redux/1-introducao-ao-redux-o-estado-global-da-aplicacao/exercicio2/index.js
@@ -0,0 +1,35 @@
+import { legacy_createStore as createStore } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
+
+const THEME_INITIAL_STATE = {
+  theme: 'dark',
+};
+
+const themeReducer = (state = THEME_INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const STATUS_INITIAL_STATE = {
+  status: 'offline',
+};
+
+const statusReducer = (state = STATUS_INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const themeButton = document.getElementById('toggle-theme');
+const statusButton = document.getElementById('toggle-status');
+
+themeButton.addEventListener('click', () => {
+  // coloque o código aqui.
+});
+
+statusButton.addEventListener('click', () => {
+  // coloque o código aqui.
+});
