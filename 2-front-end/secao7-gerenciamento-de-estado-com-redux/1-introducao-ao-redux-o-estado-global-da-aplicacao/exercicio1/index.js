c202bf0fd149960fc6ef233160fd5e95db421288
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-11-29 13:25:05 -0300

    Adiciona eventListener nos botões precious e next e realiza um dispatch com as devidas actions

--- 2-front-end/secao7-gerenciamento-de-estado-com-redux/1-introducao-ao-redux-o-estado-global-da-aplicacao/exercicio1/index.js
+++ 2-front-end/secao7-gerenciamento-de-estado-com-redux/1-introducao-ao-redux-o-estado-global-da-aplicacao/exercicio1/index.js
@@ -8,13 +8,27 @@ const INITIAL_STATE = {
 
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'NEXT_COLOR':
+    case "NEXT_COLOR":
       return { index: state.index + 1 };
-    case 'PREVIOUS_COLOR':
+    case "PREVIOUS_COLOR":
       return { index: state.index - 1 };
     default:
       return state;
   };
 };
 
-const state = createStore(reducer, composeWithDevTools());
+const store = createStore(reducer, composeWithDevTools());
+
+const actionPrevious = { type: "PREVIOUS_COLOR" };
+
+const actionNext = { type: "NEXT_COLOR" };
+
+const previousButton = document.querySelector("#previous");
+previousButton.addEventListener("click", () => {
+  store.dispatch(actionPrevious);
+});
+
+const nextButton = document.querySelector("#next");
+nextButton.addEventListener("click", () => {
+  store.dispatch(actionNext);
+});
