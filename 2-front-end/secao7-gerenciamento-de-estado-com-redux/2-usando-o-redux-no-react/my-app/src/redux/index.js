f985ae510fb1d9618c67924d5582bf6e8df1acd4
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-11-30 13:16:59 -0300

    Criação de reducer, action, dispatch

--- 2-front-end/secao7-gerenciamento-de-estado-com-redux/2-usando-o-redux-no-react/my-app/src/redux/index.js
+++ 2-front-end/secao7-gerenciamento-de-estado-com-redux/2-usando-o-redux-no-react/my-app/src/redux/index.js
@@ -1,10 +1,8 @@
 import { legacy_createStore as createStore } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
 
-const INITIAL_STATE = { count: 0 };
+import counterReducer from './reducers/counterReducer';
 
-const reducer = (state = INITIAL_STATE, action) => state;
-
-const store = createStore(reducer, composeWithDevTools());
+const store = createStore(counterReducer, composeWithDevTools());
 
 export default store;
