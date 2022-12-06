10e179bcfb50576c2cbe9aa90e702ef3dd4f878a
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-12-06 13:55:11 -0300

    Cria reducer

--- 2-front-end/secao7-gerenciamento-de-estado-com-redux/4-usando-o-redux-no-react-actions-assincronas/game-of-thunks/src/redux/store/index.js
+++ 2-front-end/secao7-gerenciamento-de-estado-com-redux/4-usando-o-redux-no-react-actions-assincronas/game-of-thunks/src/redux/store/index.js
@@ -1,6 +1,7 @@
 import {  legacy_createStore as createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
+import reducer from "../reducers";
 
-const store = createStore(composeWithDevTools());
+const store = createStore(reducer, composeWithDevTools());
 
 export default store;
