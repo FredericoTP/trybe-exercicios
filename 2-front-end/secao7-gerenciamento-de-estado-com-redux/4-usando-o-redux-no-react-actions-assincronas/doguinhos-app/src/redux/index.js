80dbbc035b8e7eb71ad322ef22ad7314855cf912
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-12-06 11:40:25 -0300

    Adiciona redux-thunk

--- 2-front-end/secao7-gerenciamento-de-estado-com-redux/4-usando-o-redux-no-react-actions-assincronas/doguinhos-app/src/redux/index.js
+++ 2-front-end/secao7-gerenciamento-de-estado-com-redux/4-usando-o-redux-no-react-actions-assincronas/doguinhos-app/src/redux/index.js
@@ -1,7 +1,11 @@
-import { createStore } from 'redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
-import dogReducer from './reducers/dogReducer'
+import thunk from 'redux-thunk';
+import dogReducer from './reducers/dogReducer';
 
-const store = createStore(dogReducer, composeWithDevTools());
+const store = createStore(
+  dogReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
