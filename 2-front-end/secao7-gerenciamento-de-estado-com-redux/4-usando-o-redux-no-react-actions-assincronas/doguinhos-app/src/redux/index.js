1ba3d8388e7f42f169ab3e1be13d9ae13a6b8af9
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-12-06 11:31:06 -0300

    Redux - cria store

---
+++ 2-front-end/secao7-gerenciamento-de-estado-com-redux/4-usando-o-redux-no-react-actions-assincronas/doguinhos-app/src/redux/index.js
@@ -0,0 +1,7 @@
+import { createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import dogReducer from './reducers/dogReducer'
+
+const store = createStore(dogReducer, composeWithDevTools());
+
+export default store;
