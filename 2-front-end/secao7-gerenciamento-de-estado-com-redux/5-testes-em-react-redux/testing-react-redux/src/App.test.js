4062c1fd3fa66945fcf00c1caa7336e397a945f0
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-12-07 13:01:37 -0300

    Cria renderWithRedux

--- 2-front-end/secao7-gerenciamento-de-estado-com-redux/5-testes-em-react-redux/testing-react-redux/src/App.test.js
+++ 2-front-end/secao7-gerenciamento-de-estado-com-redux/5-testes-em-react-redux/testing-react-redux/src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { legacy_createStore as createStore } from 'redux';
+import counterReducer from './redux/reducers/counterReducer';
+import { Provider } from 'react-redux';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const renderWithRedux = (
+  component,
+  {
+    initialState,
+    store = createStore(counterReducer, initialState)
+  } = {},
+) => {
+  return {
+    ...render(<Provider store={store}>{component}</Provider>),
+    store,
+  };
+};
