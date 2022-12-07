d1d68e6cfce1c7fa398190fea4bdece4b6d3aa2a
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-12-07 13:38:16 -0300

    Adiciona teste dos botões

--- 2-front-end/secao7-gerenciamento-de-estado-com-redux/5-testes-em-react-redux/testing-react-redux/src/App.test.js
+++ 2-front-end/secao7-gerenciamento-de-estado-com-redux/5-testes-em-react-redux/testing-react-redux/src/App.test.js
@@ -1,14 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore, combineReducers } from 'redux';
 import counterReducer from './redux/reducers/counterReducer';
 import { Provider } from 'react-redux';
+import userEvent from '@testing-library/user-event';
 
 const renderWithRedux = (
   component,
-  {
+  { 
     initialState,
-    store = createStore(counterReducer, initialState)
+    store = createStore(combineReducers({ counterReducer }), initialState) 
   } = {},
 ) => {
   return {
@@ -16,3 +17,26 @@ const renderWithRedux = (
     store,
   };
 };
+
+describe('verifica a aplicação', () => {
+  it('Verifica se o botão incrementa corretamente o valor do estado global', () => {
+    const { store } = renderWithRedux(<App />);
+    expect(screen.getByText('0')).toBeInTheDocument();
+    expect(store.getState().counterReducer.count).toBe(0);
+
+    const btnPlus1 = screen.getByRole('button', {  name: /incrementa 1/i});
+    expect(btnPlus1).toBeInTheDocument();
+
+    userEvent.click(btnPlus1);
+    expect(screen.getByText('1')).toBeInTheDocument();
+    expect(store.getState().counterReducer.count).toBe(1);
+
+    const btnPlus5 = screen.getByRole('button', {  name: /incrementa 5/i});
+    expect(btnPlus5).toBeInTheDocument();
+
+    userEvent.click(btnPlus5);
+    expect(screen.getByText('6')).toBeInTheDocument();
+    expect(store.getState().counterReducer.count).toBe(6);
+  });
+  
+});
