e03904535a32289ccada8141de8baad0c2c0d3a4
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-03-07 15:27:43 -0300

    Feat: Exercise 2

--- 3-back-end/secao4-introducao-ao-desenvolvimento-web-com-node-js/1-node-js-runtime-assincrono/exercises/bmi.js
+++ 3-back-end/secao4-introducao-ao-desenvolvimento-web-com-node-js/1-node-js-runtime-assincrono/exercises/bmi.js
@@ -1,7 +1,10 @@
 function bmiCalculator(weight, height) {
   const bmi = weight / (height * height);
+  console.log(bmi);
 
   return bmi;
 }
+
+bmiCalculator(70, 1.74);
 
 module.exports = { bmiCalculator };
