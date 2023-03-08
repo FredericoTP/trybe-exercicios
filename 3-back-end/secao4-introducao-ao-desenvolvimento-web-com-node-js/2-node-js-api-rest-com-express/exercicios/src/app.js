30e89c0d06ad02577d95870634ff0442470cf1c2
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-03-08 16:25:16 -0300

    Feat: Exercício 5

--- 3-back-end/secao4-introducao-ao-desenvolvimento-web-com-node-js/2-node-js-api-rest-com-express/exercicios/src/app.js
+++ 3-back-end/secao4-introducao-ao-desenvolvimento-web-com-node-js/2-node-js-api-rest-com-express/exercicios/src/app.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const { readMovies } = require('./movies');
 
 const app = express();
+
+app.get('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+  const movies = await readMovies();
+  const movieById = movies.filter((item) => item.id === Number(id));
+
+  if (movieById.length === 0) return res.status(404).json({ message: 'Movie not found' });
+
+  return res.status(200).json(movieById[0]);
+});
 
 module.exports = app;
