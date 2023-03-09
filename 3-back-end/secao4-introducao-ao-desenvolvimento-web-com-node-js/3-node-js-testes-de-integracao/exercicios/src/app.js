9e43dc8a8eec2c20411052774d0007c4c9345c84
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-03-09 15:51:02 -0300

    Feat: arquivos para exercicios

---
+++ 3-back-end/secao4-introducao-ao-desenvolvimento-web-com-node-js/3-node-js-testes-de-integracao/exercicios/src/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { getAllChocolates, getChocolateById, getChocolatesByBrand } = require('./cacauTrybe');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/chocolates', async (_req, res) => {
+  const chocolates = await getAllChocolates();
+
+  if (!chocolates) return res.status(404).json({ message: 'Algo inesperado aconteceu!' });
+
+  return res.status(200).json({ chocolates });
+});
+
+app.get('/chocolates/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const chocolate = await getChocolateById(Number(id));
+
+  if (!chocolate) return res.status(404).json({ message: 'Chocolate não encontrado!' });
+
+  return res.status(200).json({ chocolate });
+});
+
+app.get('/chocolates/brand/:brandId', async (req, res) => {
+  const { brandId } = req.params;
+
+  const chocolates = await getChocolatesByBrand(Number(brandId));
+
+  if (chocolates.length === 0) {
+    return res.status(404).json({ message: 'Chocolate não encontrado!' });
+  }
+
+  return res.status(200).json({ chocolates });
+});
+
+module.exports = app;
---
+++ 3-back-end/secao4-introducao-ao-desenvolvimento-web-com-node-js/3-node-js-testes-de-integracao/exercicios/src/cacauTrybe.js
@@ -0,0 +1,40 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function readCacauTrybeFile() {
+  const cacauTrybePath = path.resolve(__dirname, './files/cacauTrybeFile.json');
+
+  try {
+    const response = await fs.readFile(cacauTrybePath, 'utf-8');
+    const data = JSON.parse(response);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
+}
+
+async function getAllChocolates() {
+  const cacauTrybe = await readCacauTrybeFile();
+  return cacauTrybe.chocolates;
+}
+
+async function getChocolateById(id) {
+  const cacauTrybe = await readCacauTrybeFile();
+  const chocolateById = cacauTrybe.chocolates.find((item) => item.id === id);
+
+  return chocolateById;
+}
+
+async function getChocolatesByBrand(brandId) {
+  const cacauTrybe = await readCacauTrybeFile();
+  const chocolatesByBrand = cacauTrybe.chocolates.filter((item) => item.brandId === brandId);
+
+  return chocolatesByBrand;
+}
+
+module.exports = {
+  getAllChocolates,
+  getChocolateById,
+  getChocolatesByBrand,
+};
