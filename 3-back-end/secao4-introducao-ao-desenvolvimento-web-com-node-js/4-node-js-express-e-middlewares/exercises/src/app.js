0688fdc1914dc67a6c0fbe5109c5bfd1a803c31f
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-03-10 16:29:03 -0300

    Feat: exercício 4

--- 3-back-end/secao4-introducao-ao-desenvolvimento-web-com-node-js/4-node-js-express-e-middlewares/exercises/src/app.js
+++ 3-back-end/secao4-introducao-ao-desenvolvimento-web-com-node-js/4-node-js-express-e-middlewares/exercises/src/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { addNewLocation } = require('./ecoturism');
+const nameValidation = require('./middleware/nameValidation');
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/activities', (req, res) => {
+app.post('/activities', nameValidation, (req, res) => {
   const object = req.body;
 
   addNewLocation(object);
