19de86814bd8ea4e357a1504504dfca7ccbe5be3
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-03-27 15:50:05 -0300

    Feat: Exercício 7

--- 3-back-end/secao6-nodejs-orm-e-autentificacao/1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercise-books-api/controller/book.controller.js
+++ 3-back-end/secao6-nodejs-orm-e-autentificacao/1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercise-books-api/controller/book.controller.js
@@ -1,6 +1,8 @@
 const BookService = require('../services/book.service');
 
-const getAll = async (req, res) => {
+const error500Message = 'Algo deu errado';
+
+const getAll = async (_req, res) => {
   try {
     const books = await BookService.getAll();
     return res.status(200).json(books);
@@ -10,6 +12,21 @@ const getAll = async (req, res) => {
   }
 }
 
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await BookService.getById(id);
+
+    if (!book) return res.status(404).json({ message: "Book not found" });
+
+    return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error500Message });
+  }
+}
+
 module.exports = {
   getAll,
+  getById,
 }
