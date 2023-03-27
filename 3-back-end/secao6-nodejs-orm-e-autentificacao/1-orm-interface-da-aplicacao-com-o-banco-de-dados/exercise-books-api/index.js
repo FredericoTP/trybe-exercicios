eb74b2adbc97b37328b7af597c973dafc587b84a
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-03-27 15:42:45 -0300

    Feat: Exercício 5

--- 3-back-end/secao6-nodejs-orm-e-autentificacao/1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercise-books-api/index.js
+++ 3-back-end/secao6-nodejs-orm-e-autentificacao/1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercise-books-api/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const BookController = require('./controller/book.controller');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.get('/book', BookController.getAll);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
