b2cc4e5a7a4811c086fadcb5aeda47c307f9d99c
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-03-27 16:03:52 -0300

    Feat: Exercício 13

--- 3-back-end/secao6-nodejs-orm-e-autentificacao/1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercise-books-api/controller/book.controller.js
+++ 3-back-end/secao6-nodejs-orm-e-autentificacao/1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercise-books-api/controller/book.controller.js
@@ -38,8 +38,22 @@ const createBook = async (req, res) => {
   }
 }
 
+const updateBook = async (req, res) => {
+  try {
+    const { title, author, pageQuantity } = req.body;
+    const { id } = req.prams;
+    const updatedBook = await BookService.updateBook(id, title, author, pageQuantity);
+
+    return res.status(200).json({ message: "Book updated!" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error500Message });
+  }
+}
+
 module.exports = {
   getAll,
   getById,
   createBook,
+  updateBook,
 }
