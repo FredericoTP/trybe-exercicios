02ec3fda140cf3b40a1da753b738a0b979985ec1
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-04-03 11:47:35 -0300

    Feat: Adiciona o endpoint post em employee

---
+++ 3-back-end/secao6-nodejs-orm-e-autentificacao/3-orm-associations-n_n-e-transactions/practice-transactions/src/services/employee.service.js
@@ -0,0 +1,41 @@
+const { Address, Employee } = require('../models/');
+
+const getAll = async () => {
+  const users = await Employee.findAll({
+    include: { model: Address, as: 'addresses' },
+  });
+
+  return users;
+};
+
+// Eager Loading
+
+// const getById = async (id) => {
+//   const employee = await Employee.findOne({
+//     where: { id },
+//     include: [{
+//       model: Address, as: 'addresses',
+//       attributes: { exclude: ['number'] },
+//     }],
+//   });
+//   return employee;
+// }
+
+// Lazy Loading
+
+const getById = async (id) => {
+  const employee = await Employee.findOne({
+    where: { id },
+  });
+  return employee;
+}
+
+const insert = async ({ firstName, lastName, age, city, street, number }) => {
+  const employee = await Employee.create({ firstName, lastName, age });
+
+  await Address.create({ city, street, number, employeeId: employee.id });
+
+  return employee;
+};
+
+module.exports = { getAll, getById, insert };
