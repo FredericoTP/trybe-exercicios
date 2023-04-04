b422a9f34f134648506a3db55c59c8c4e4a238a9
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-04-04 13:11:22 -0300

    Feat: Exercise 3

--- 3-back-end/secao6-nodejs-orm-e-autentificacao/4-JWT-JSON-Web-Token/exercise/hello-jwt/src/app.js
+++ 3-back-end/secao6-nodejs-orm-e-autentificacao/4-JWT-JSON-Web-Token/exercise/hello-jwt/src/app.js
@@ -22,6 +22,8 @@ app.get('/ping', controllers.ping);
 
 app.post('/login', controllers.login);
 
+app.get('/users/me', middlewares.auth, controllers.me);
+
 app.use(middlewares.error);
 
 module.exports = app;
