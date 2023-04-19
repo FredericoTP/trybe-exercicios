7760f117ba4996da5008fe4ee6c5c598735eee8a
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-04-19 13:05:41 -0300

    Feat: Practice Type Union

---
+++ 3-back-end/secao8-introducao-a-typescript/2-tipagem-estatica-e-generics/practice/typeUnion.ts
@@ -0,0 +1,10 @@
+type EstadosDaMateria = 'líquido' | 'sólido' | 'gasoso';
+
+type numberCPF = string | number;
+
+const cpf: numberCPF = 123456789012;
+const cpf2: numberCPF = '123.567.890-12';
+
+type SistemasOperacionais = 'linux' | 'mac' | ' windows';
+
+type Vogais = 'a' | 'e' | 'i' | 'o' | 'u';
