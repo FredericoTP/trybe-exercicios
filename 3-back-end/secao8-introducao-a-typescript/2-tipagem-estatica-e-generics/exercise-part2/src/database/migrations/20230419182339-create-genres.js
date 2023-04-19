b613efc1bcb963ad53ae4db65400ae4cfeac3531
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-04-19 15:26:29 -0300

    Feat: Exercise-Part2 2

---
+++ 3-back-end/secao8-introducao-a-typescript/2-tipagem-estatica-e-generics/exercise-part2/src/database/migrations/20230419182339-create-genres.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('genres', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      genre: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      }
+    })
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('genres');
+  }
+};
