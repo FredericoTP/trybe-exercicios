0ff8007bb29b595407dd6acc7b79e38d22998ddb
Frederico Tavares <fredericotp@hotmail.com>
2023-04-27 15:16:46 -0300

    Feat: Exercise 3

---
+++ 3-back-end/secao9-programacao-orientada-a-objetos-poo-e-solid/1-introducao-a-orientacao-a-objetos/exercise/snackBar.ts
@@ -0,0 +1,87 @@
+class Customer {
+  private _name: string;
+
+  constructor(n: string) {
+    this._name = n;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+}
+
+class menuItem {
+  private _name: string;
+  private _price: number;
+
+  constructor(n: string, p: number) {
+    this._name = n;
+    this._price = p;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+
+  get price(): number {
+    return this._price;
+  }
+
+  set price(value: number) {
+    this._price = value;
+  }
+}
+
+class Order {
+  private _customer: Customer;
+  private _consumption: menuItem[];
+  private _paymentMethod: string;
+  private _discount: number;
+
+  constructor(cus: Customer, con: menuItem[], p: string, d: number) {
+    this._customer = cus;
+    this._consumption = con;
+    this._paymentMethod = p;
+    this._discount = d;
+  }
+
+  get customer(): Customer {
+    return this._customer;
+  }
+
+  set customer(value: Customer) {
+    this._customer = value;
+  }
+
+  get consumption(): menuItem[] {
+    return this._consumption;
+  }
+
+  set consumption(value: menuItem[]) {
+    this._consumption = value;
+  }
+
+  get paymentMethod(): string {
+    return this._paymentMethod;
+  }
+
+  set paymentMethod(value: string) {
+    this._paymentMethod = value;
+  }
+
+  get discount(): number {
+    return this._discount;
+  }
+
+  set discount(value: number) {
+    this._discount = value;
+  }
+}
