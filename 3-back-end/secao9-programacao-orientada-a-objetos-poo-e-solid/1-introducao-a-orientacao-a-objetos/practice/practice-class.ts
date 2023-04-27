18790dac16041bbf71a0ccc6be1f4720e673b20a
Frederico Tavares <fredericotp@hotmail.com>
2023-04-27 13:19:40 -0300

    Feat: Practice Class exercise 4-5

--- 3-back-end/secao9-programacao-orientada-a-objetos-poo-e-solid/1-introducao-a-orientacao-a-objetos/practice/practice-class.ts
+++ 3-back-end/secao9-programacao-orientada-a-objetos-poo-e-solid/1-introducao-a-orientacao-a-objetos/practice/practice-class.ts
@@ -1,20 +1,20 @@
 class Tv {
-  brand: string;
-  size: number;
-  resolution: string;
-  connections: string[];
-  connectedTo?: string;
+  private _brand: string;
+  private _size: number;
+  private _resolution: string;
+  private _connections: string[];
+  private _connectedTo?: string;
 
   constructor(b: string, s: number, r: string, c: string[]) {
-    this.brand = b;
-    this.size = s;
-    this.resolution = r;
-    this.connections = c;
+    this._brand = b;
+    this._size = s;
+    this._resolution = r;
+    this._connections = c;
   }
 
   turnOn(): void {
-    console.log(`TV ${this.brand}, ${this.size} inches, resolution ${this.resolution}.
-    Available connections: ${this.connections}`);
+    console.log(`TV ${this._brand}, ${this._size} inches, resolution ${this._resolution}.
+    Available connections: ${this._connections}`);
   }
 }
 
