b76824eec5db9c57d4cf3c8b1c1e9831385f5110
Frederico Tavares <fredericotp@hotmail.com>
2023-04-28 13:15:45 -0300

    Feat: Practice Herança 2

--- 3-back-end/secao9-programacao-orientada-a-objetos-poo-e-solid/2-herança-e-composicao/practice/heranca-practice.ts
+++ 3-back-end/secao9-programacao-orientada-a-objetos-poo-e-solid/2-herança-e-composicao/practice/heranca-practice.ts
@@ -1,21 +1,23 @@
 class Superclass {
   constructor(public isSuper: boolean = true) { }
 
-  sayHello() {
+  protected sayHello() {
     console.log('Olá mundo!');
   }
 }
 
 class Subclass extends Superclass {
-
+  sayHello2() {
+    this.sayHello();
+  }
 }
 
-function myFunc(object: Superclass) {
-  object.sayHello();
+function myFunc(object: Subclass) {
+  object.sayHello2();
 }
 
-const objeto1 = new Superclass();
+// const objeto1 = new Superclass();
 const objeto2 = new Subclass();
 
-myFunc(objeto1);
+// myFunc(objeto1);
 myFunc(objeto2);
