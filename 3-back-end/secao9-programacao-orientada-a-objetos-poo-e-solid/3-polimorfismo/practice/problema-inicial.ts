5ecb19c18c3950face44b06109a6bec67ba263b3
Frederico Tavares <fredericotp@hotmail.com>
2023-05-02 11:45:41 -0300

    Feat: Practice - Problema Inicial - Classe abstrata da peça

--- 3-back-end/secao9-programacao-orientada-a-objetos-poo-e-solid/3-polimorfismo/practice/problema-inicial.ts
+++ 3-back-end/secao9-programacao-orientada-a-objetos-poo-e-solid/3-polimorfismo/practice/problema-inicial.ts
@@ -7,3 +7,24 @@ const boardRows: BoardRow[] = ['1', '2', '3', '4', '5', '6', '7', '8'];
 
 const isBoardSquareInList = (element: BoardSquare, list: BoardSquare[]) =>
   list.some(e => element[0] === e[0] && element[1] === e[1]);
+
+abstract class Piece {
+  protected _position: BoardSquare;
+  abstract type: string;
+
+  constructor(position: BoardSquare, protected board: Board) {
+    this._position = position;
+  };
+
+  get position() { return this._position; }
+  abstract get availableMoves(): BoardSquare[];
+
+  move(newPosition: BoardSquare) {
+    console.log(
+      `MOVENDO ${this.type} DA CASA ${this._position} PARA A CASA ${newPosition}`
+    );
+    if (!isBoardSquareInList(newPosition, this.availableMoves)) return false;
+    this._position = newPosition;
+    return true;
+  }
+}
