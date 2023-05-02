8d850c499affda798bbb704bc6bf59af80b88dfd
Frederico Tavares <fredericotp@hotmail.com>
2023-05-02 11:49:13 -0300

    Feat: Practice - Problema Inicial - Classe tabuleiro

--- 3-back-end/secao9-programacao-orientada-a-objetos-poo-e-solid/3-polimorfismo/practice/problema-inicial.ts
+++ 3-back-end/secao9-programacao-orientada-a-objetos-poo-e-solid/3-polimorfismo/practice/problema-inicial.ts
@@ -91,3 +91,22 @@ class Rook extends Piece {
     return availableSquares;
   }
 }
+
+class Board {
+  pieces: Piece[] = [];
+
+  constructor() {
+    this.addPiece(new Pawn(['C', '2'], this));
+    this.addPiece(new Rook(['F', '1'], this));
+  }
+
+  private addPiece(piece: Piece) {
+    if (isBoardSquareInList(piece.position, this.occupiedSquares))
+      return;
+    this.pieces.push(piece);
+  }
+  get occupiedSquares() {
+    return this.pieces.map((piece) => piece.position);
+  }
+
+}
