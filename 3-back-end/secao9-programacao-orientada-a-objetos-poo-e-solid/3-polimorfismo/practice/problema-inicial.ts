a42fa287b22354e78ff14648c83e4b98bcea76e1
Frederico Tavares <fredericotp@hotmail.com>
2023-05-02 11:46:37 -0300

    Feat: Practice - Problema Inicial - Classe peão

--- 3-back-end/secao9-programacao-orientada-a-objetos-poo-e-solid/3-polimorfismo/practice/problema-inicial.ts
+++ 3-back-end/secao9-programacao-orientada-a-objetos-poo-e-solid/3-polimorfismo/practice/problema-inicial.ts
@@ -28,3 +28,20 @@ abstract class Piece {
     return true;
   }
 }
+
+class Pawn extends Piece {
+  type = 'Peão';
+  get availableMoves() {
+    const column = this.position[0];
+    const row = this.position[1];
+    const rowIndex = boardRows.indexOf(row);
+    if (rowIndex === 7)
+      return [];
+
+    const nextSquare: BoardSquare = [column, boardRows[rowIndex + 1]];
+    if (isBoardSquareInList(nextSquare, this.board.occupiedSquares))
+      return [];
+
+    return [nextSquare];
+  }
+}
