7518202f1ed17c2baeb4a6eafe04be093a65e7ef
Frederico Tavares <fredericotp@hotmail.com>
2023-05-02 11:48:00 -0300

    Feat: Practice - Problema Inicial - Classe torre

--- 3-back-end/secao9-programacao-orientada-a-objetos-poo-e-solid/3-polimorfismo/practice/problema-inicial.ts
+++ 3-back-end/secao9-programacao-orientada-a-objetos-poo-e-solid/3-polimorfismo/practice/problema-inicial.ts
@@ -45,3 +45,49 @@ class Pawn extends Piece {
     return [nextSquare];
   }
 }
+
+class Rook extends Piece {
+  type = "Torre";
+  get availableMoves() {
+    const column = this.position[0];
+    const columnIndex = boardColumns.indexOf(column);
+
+    const row = this.position[1];
+    const rowIndex = boardRows.indexOf(row);
+
+    let availableSquares: BoardSquare[] = [];
+
+    // Adiciona todas as casas abaixo
+    for (let i = rowIndex - 1; i >= 0; i--) {
+      const square: BoardSquare = [boardColumns[columnIndex], boardRows[i]];
+      if (isBoardSquareInList(square, this.board.occupiedSquares)) break;
+      availableSquares.push(square);
+    }
+
+    // Adiciona todas as casas acima
+    for (let i = rowIndex + 1; i < 8; i++) {
+      const square: BoardSquare = [boardColumns[columnIndex], boardRows[i]];
+      if (isBoardSquareInList(square, this.board.occupiedSquares)) break;
+      if (isBoardSquareInList(square, availableSquares)) continue;
+      availableSquares.push(square);
+    }
+
+    // Adiciona todas as casas na direita
+    for (let i = columnIndex + 1; i < 8; i++) {
+      const square: BoardSquare = [boardColumns[i], boardRows[rowIndex]];
+      if (isBoardSquareInList(square, this.board.occupiedSquares)) break;
+      if (isBoardSquareInList(square, availableSquares)) continue;
+      availableSquares.push(square);
+    }
+
+    // Adiciona todas as casas na esquerda
+    for (let i = columnIndex - 1; i >= 0; i--) {
+      const square: BoardSquare = [boardColumns[i], boardRows[rowIndex]];
+      if (isBoardSquareInList(square, this.board.occupiedSquares)) break;
+      if (isBoardSquareInList(square, availableSquares)) continue;
+      availableSquares.push(square);
+    }
+
+    return availableSquares;
+  }
+}
