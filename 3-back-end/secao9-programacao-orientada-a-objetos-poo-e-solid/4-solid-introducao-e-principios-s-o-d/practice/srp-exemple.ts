5dd54e5ee3493357c5ae51a0f7c85bd6b7b5c6a3
Frederico Tavares <fredericotp@hotmail.com>
2023-05-08 11:08:10 -0300

    Feat: Practice srp

--- 3-back-end/secao9-programacao-orientada-a-objetos-poo-e-solid/4-solid-introducao-e-principios-s-o-d/practice/srp-exemple.ts
+++ 3-back-end/secao9-programacao-orientada-a-objetos-poo-e-solid/4-solid-introducao-e-principios-s-o-d/practice/srp-exemple.ts
@@ -1,47 +1,64 @@
+function progressNotification(message: string): void {
+  console.log(message);
+}
+
 type Book = {
   book: string;
   author: string;
   genre: string;
+};
+
+class BooksWishlist {
+  private wishlist: Book[];
+
+  constructor(book: Book) {
+    this.wishlist = [];
+    this.wishlist.push(book);
+  }
+
+  addToWishlist(newBook: Book): void {
+    this.wishlist.push(newBook);
+  }
+
+  showWishlist(): void {
+    return console.log(this.wishlist);
+  }
 }
+
+const wishlist = new BooksWishlist({
+  book: 'The Road',
+  author: 'Cormac McCarthy',
+  genre: 'Dystopia',
+});
+wishlist.addToWishlist({
+  book: 'Misery',
+  author: 'Stephen King',
+  genre: 'Thriller',
+});
+wishlist.showWishlist();
 
 class ReadingTracker {
   private readingGoal: number;
   private booksRead: number;
-  private wishlist: Book[];
   constructor(readingGoal: number) {
     this.readingGoal = readingGoal;
     this.booksRead = 0;
-    this.wishlist = [];
   }
 
   trackReadings(readsCount: number): void {
     this.booksRead += readsCount;
     if (this.booksRead >= this.readingGoal) {
-      this.progressNotification(
+      progressNotification(
         'Congratulations! You\'ve reached your reading goal!',
       );
       return;
     }
-    this.progressNotification(
+    progressNotification(
       'There are still some books to go!',
     );
-  }
-
-  addToWishlist(book: Book): void {
-    this.wishlist.push(book);
-  }
-
-  showWishlist(): void {
-    console.log(this.wishlist);
-  }
-
-  progressNotification(message: string): void {
-    console.log(message);
   }
 }
 
 const readTracker = new ReadingTracker(20);
-readTracker.addToWishlist({ book: 'The Road', author: 'Cormac McCarthy', genre: 'Dystopia' });
-readTracker.showWishlist();
 readTracker.trackReadings(12);
 readTracker.trackReadings(9);
