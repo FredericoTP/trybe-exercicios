ee44c9139a47e8e2e661277b0820258899798839
Frederico Tavares <fredericotp@hotmail.com>
2023-05-08 11:13:54 -0300

    Feat: Practice ocp

--- 3-back-end/secao9-programacao-orientada-a-objetos-poo-e-solid/4-solid-introducao-e-principios-s-o-d/practice/ocp-example.ts
+++ 3-back-end/secao9-programacao-orientada-a-objetos-poo-e-solid/4-solid-introducao-e-principios-s-o-d/practice/ocp-example.ts
@@ -1,19 +1,39 @@
-function progressNotification(
+type Notification = {
+  type: string,
+  send: (message: string) => void
+};
+
+// Criando variáveis para guardar cada tipo aceito por nossa função
+const consoleNotification: Notification = {
+  type: 'Console',
+  send: (message) => console.log('Console: ', message),
+};
+
+const emailNotification: Notification = {
+  type: 'Email',
+  send: (message) => console.log('Email: ', message),
+};
+
+const phoneNotification: Notification = {
+  type: 'Phone',
+  send: (message) => console.log('Phone: ', message),
+};
+
+const notifications: Notification[] = [
+  consoleNotification,
+  emailNotification,
+  phoneNotification,
+];
+
+// Agora podemos adicionar novos tipos sem modificar nossa função
+export default function progressNotification(
   message: string,
-  notificationType: string,
+  type_ = 'Console',
+  notificationsArray: Notification[] = notifications,
 ): void {
-  switch (notificationType) {
-    case 'Email':
-      console.log('Email: ', message);
-      break;
-    case 'Phone':
-      console.log('Phone: ', message);
-      break;
-    case 'Console':
-      console.log('Console: ', message);
-      break;
-    default:
-      console.log(message);
-      break;
-  }
+  notificationsArray.forEach((notification) => {
+    if (notification.type === type_) {
+      notification.send(message);
+    }
+  });
 }
