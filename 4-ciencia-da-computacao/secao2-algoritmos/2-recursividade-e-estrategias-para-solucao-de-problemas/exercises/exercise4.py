884a503c9ca9f02f9c69328a112eed8e73c45ad1
Frederico Tavares <fredericotp@hotmail.com>
2023-05-30 12:47:51 -0300

    Feat: Exercise 4

---
+++ 4-ciencia-da-computacao/secao2-algoritmos/2-recursividade-e-estrategias-para-solucao-de-problemas/exercises/exercise4.py
@@ -0,0 +1,10 @@
+# Escreva um algoritmo recursivo para encontrar o máximo divisor comum (mdc) de dois inteiros.
+
+
+def mdc(a, b):
+    if b == 0:
+        return a
+    return mdc(b, a % b)
+
+
+print(mdc(8, 6))
