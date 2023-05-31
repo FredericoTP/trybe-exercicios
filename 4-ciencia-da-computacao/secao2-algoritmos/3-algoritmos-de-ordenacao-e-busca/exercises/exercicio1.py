9350222193d0d3739024aa69f4d860514d6e5e88
Frederico Tavares <fredericotp@hotmail.com>
2023-05-31 11:43:30 -0300

    Feat: Exercício 1

---
+++ 4-ciencia-da-computacao/secao2-algoritmos/3-algoritmos-de-ordenacao-e-busca/exercises/exercicio1.py
@@ -0,0 +1,6 @@
+# Dado um array com os seguintes elementos ["zebra", "macaco", "elefante", "arara", "javali"], após duas iterações utilizando bubble sort, como estaria este array?
+
+
+# Apoś duas iterações o array estaria da seguinte forma:
+
+animais = ["elefante", "arara", "javali", "macaco", "zebra"]
