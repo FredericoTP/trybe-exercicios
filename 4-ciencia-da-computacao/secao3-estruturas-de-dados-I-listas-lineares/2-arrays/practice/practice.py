557294204370f1584a06c7148d9ed1fa7d835fe4
Frederico Tavares <fredericotp@hotmail.com>
2023-06-06 11:41:14 -0300

    Feat: Practice 3

--- 4-ciencia-da-computacao/secao3-estruturas-de-dados-I-listas-lineares/2-arrays/practice/practice.py
+++ 4-ciencia-da-computacao/secao3-estruturas-de-dados-I-listas-lineares/2-arrays/practice/practice.py
@@ -5,3 +5,29 @@
 # 2- Listas(list), dicionários(dict) e conjuntos(set), que já vêm no Python, são considerados tipos de dados? Caso negativo, justifique sua resposta.
 
 # Sim.
+
+# 3- Que tal adicionarmos um método update que atualiza o valor a partir de um índice?
+# A assinatura deve ser def update(self, index, value):
+
+
+class ListArray:
+    def __init__(self):
+        self.data = []
+
+    def len(self):
+        return len(self.data)
+
+    def __str__(self):
+        return str(self.data)
+
+    def get(self, index):
+        return self.data[index]
+
+    def set(self, index, value):
+        return self.data.insert(index, value)
+
+    def remove(self, index):
+        return self.data.pop(index)
+
+    def update(self, index, value):
+        self.data[index] = value
