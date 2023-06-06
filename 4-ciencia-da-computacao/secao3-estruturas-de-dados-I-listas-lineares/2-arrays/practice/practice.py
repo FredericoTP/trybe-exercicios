3eed5537e2294cbed66de50ef027dd5d95103b73
Frederico Tavares <fredericotp@hotmail.com>
2023-06-06 11:35:53 -0300

    Feat: Practice 2

--- 4-ciencia-da-computacao/secao3-estruturas-de-dados-I-listas-lineares/2-arrays/practice/practice.py
+++ 4-ciencia-da-computacao/secao3-estruturas-de-dados-I-listas-lineares/2-arrays/practice/practice.py
@@ -1,3 +1,7 @@
 # 1- Tipos abstratos de dados possuem uma única implementação bem documentada?
 
 # Não.
+
+# 2- Listas(list), dicionários(dict) e conjuntos(set), que já vêm no Python, são considerados tipos de dados? Caso negativo, justifique sua resposta.
+
+# Sim.
