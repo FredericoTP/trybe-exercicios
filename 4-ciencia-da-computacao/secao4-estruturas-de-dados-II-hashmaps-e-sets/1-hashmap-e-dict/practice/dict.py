02d59cc8689875e68fbc4c2d65daa3c97a87e096
Frederico Tavares <fredericotp@hotmail.com>
2023-06-15 14:22:05 -0300

    Feat: Practice 6

--- 4-ciencia-da-computacao/secao4-estruturas-de-dados-II-hashmaps-e-sets/1-hashmap-e-dict/practice/dict.py
+++ 4-ciencia-da-computacao/secao4-estruturas-de-dados-II-hashmaps-e-sets/1-hashmap-e-dict/practice/dict.py
@@ -2,4 +2,22 @@
 
 double = {index: index * 2 for index in range(3, 21)}
 
-print(double)
+# print(double)
+
+# 6- Dada uma string, construa um dicionário com a contagem de cada caractere da palavra.
+
+
+def count_char(string):
+    count = {}
+
+    for letter in string:
+        if letter not in count:
+            count[letter] = 1
+        else:
+            count[letter] += 1
+
+    return count
+
+
+# print(count_char("bbbbaaaacccaaaaaaddddddddccccccc"))
+# print(count_char("coxinha"))
