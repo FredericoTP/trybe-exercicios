6a2957de71728c861e70752f2a0e8f0a61652ec1
Frederico Tavares <fredericotp@hotmail.com>
2023-06-15 14:18:35 -0300

    Feat: Practice 5

---
+++ 4-ciencia-da-computacao/secao4-estruturas-de-dados-II-hashmaps-e-sets/1-hashmap-e-dict/practice/dict.py
@@ -0,0 +1,5 @@
+# 5- Consulte a forma de se criar um dicionário chamado de dict comprehension e crie um dicionário que mapeia inteiros de 3 a 20 para o seu dobro.
+
+double = {index: index * 2 for index in range(3, 21)}
+
+print(double)
