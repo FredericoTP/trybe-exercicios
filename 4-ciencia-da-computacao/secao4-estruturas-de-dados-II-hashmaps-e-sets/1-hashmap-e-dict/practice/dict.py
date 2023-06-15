4a0c5af373762181d7d4fb70951dcaaa95eafb93
Frederico Tavares <fredericotp@hotmail.com>
2023-06-15 14:25:47 -0300

    Feat: Practice 7

--- 4-ciencia-da-computacao/secao4-estruturas-de-dados-II-hashmaps-e-sets/1-hashmap-e-dict/practice/dict.py
+++ 4-ciencia-da-computacao/secao4-estruturas-de-dados-II-hashmaps-e-sets/1-hashmap-e-dict/practice/dict.py
@@ -2,7 +2,7 @@
 
 double = {index: index * 2 for index in range(3, 21)}
 
-# print(double)
+print(double)
 
 # 6- Dada uma string, construa um dicionário com a contagem de cada caractere da palavra.
 
@@ -19,5 +19,14 @@ def count_char(string):
     return count
 
 
-# print(count_char("bbbbaaaacccaaaaaaddddddddccccccc"))
-# print(count_char("coxinha"))
+print(count_char("bbbbaaaacccaaaaaaddddddddccccccc"))
+print(count_char("coxinha"))
+
+# 7- Utilize o dicionário criado no exercício 5. Para as chaves ímpares, não queremos mais mapear para o seu dobro, mas sim para o seu triplo. Consulte o método keys() e atualize o seu dicionário para a nova regra.
+
+change_dict = double.keys()
+for num in change_dict:
+    if num % 2 == 1:
+        double[num] = num * 3
+
+print(double)
