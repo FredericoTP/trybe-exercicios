79fefe5b2ffa766cc1fdd89250bf4e6cb8f9dee6
Frederico Tavares <fredericotp@hotmail.com>
2023-06-16 10:53:39 -0300

    Feat: Practice 2

--- 4-ciencia-da-computacao/secao4-estruturas-de-dados-II-hashmaps-e-sets/2-set/practice/conjunto.py
+++ 4-ciencia-da-computacao/secao4-estruturas-de-dados-II-hashmaps-e-sets/2-set/practice/conjunto.py
@@ -3,6 +3,18 @@ class Conjunto:
         self.data = [False for i in range(size + 1)]
         self.last_element = 0
 
+    def __str__(self):
+        string = "{"
+
+        for index, is_in_set in enumerate(self.data):
+            if is_in_set:
+                string += str(index)
+                if index < self.last_element:
+                    string += ", "
+
+        string += "}"
+        return string
+
     def add(self, value):
         if not self.data[value]:
             self.data[value] = True
