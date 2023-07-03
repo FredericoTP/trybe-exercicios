19aaca53ffdf593dcefd408e3d9e3bbadaaa86f7
Frederico Tavares <fredericotp@hotmail.com>
2023-07-03 11:49:33 -0300

    Feat: Practice Fixtures

--- 5-eletiva-python/secao1-debug-com-python/2-automatizando-o-bug-hunting-com-pytest/practice/a_test.py
+++ 5-eletiva-python/secao1-debug-com-python/2-automatizando-o-bug-hunting-com-pytest/practice/a_test.py
@@ -5,5 +5,9 @@ def test_a_simple_test():
     assert True
 
 
+def test_sum(my_list):
+    assert sum(my_list) == 6
+
+
 def test_mean():
     assert mean([1, 2, 3, 4, 5]) == 3
---
+++ 5-eletiva-python/secao1-debug-com-python/2-automatizando-o-bug-hunting-com-pytest/practice/conftest.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+@pytest.fixture
+def my_list():
+    return [1, 2, 3]
