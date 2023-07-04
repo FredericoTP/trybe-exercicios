553ae4d96a02494b78c2a375a2581704072eceb8
Frederico Tavares <fredericotp@hotmail.com>
2023-07-04 10:47:35 -0300

    Feat: Practice 3 - faker com pytest

---
+++ 5-eletiva-python/secao1-debug-com-python/3-avancando-nos-testes-em-python/practice/test_create_user.py
@@ -0,0 +1,22 @@
+# Altere a função de teste test_create_user para utilizar o Faker a fim de gerar os dados de teste, em vez de dados fixos
+
+
+def create_user(name, email):
+    return {
+        "first_name": name.split()[0],
+        "last_name": name.split()[-1],
+        "email": email,
+        "email_domain": email.split("@")[-1],
+    }
+
+
+def test_create_user(faker):
+    fake_email = faker.email()
+    fake_name = faker.name()
+
+    res = create_user(fake_name, fake_email)
+
+    assert fake_name.startswith(res["first_name"])
+    assert fake_name.endswith(res["last_name"])
+    assert res["email"] == fake_email
+    assert fake_email.endswith(res["email_domain"])
