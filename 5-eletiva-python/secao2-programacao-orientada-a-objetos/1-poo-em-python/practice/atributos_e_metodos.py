3fd4d3d452a0573d1083d4050a8f7bb212c77389
Frederico Tavares <fredericotp@hotmail.com>
2023-07-07 11:17:20 -0300

    Feat: Practice 5 - Atributos e Métodos

---
+++ 5-eletiva-python/secao2-programacao-orientada-a-objetos/1-poo-em-python/practice/atributos_e_metodos.py
@@ -0,0 +1,12 @@
+# 5- Cria uma classe Carro e, depois, atribua valores aos atributos marca, modelo, ano e cor.
+
+
+class Carro:
+    pass
+
+
+meu_carro = Carro()
+meu_carro.marca = "Fiat"
+meu_carro.modelo = "Uno de escada"
+meu_carro.ano = 2000
+meu_carro.cor = "Vermelho"
