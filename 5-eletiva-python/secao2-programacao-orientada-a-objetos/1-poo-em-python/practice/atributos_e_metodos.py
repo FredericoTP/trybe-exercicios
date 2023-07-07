ad93ed792f2de446f11b3969dbe9cb9f1e1f3967
Frederico Tavares <fredericotp@hotmail.com>
2023-07-07 11:23:13 -0300

    Feat: Practice 6 - Atributos e Métodos

--- 5-eletiva-python/secao2-programacao-orientada-a-objetos/1-poo-em-python/practice/atributos_e_metodos.py
+++ 5-eletiva-python/secao2-programacao-orientada-a-objetos/1-poo-em-python/practice/atributos_e_metodos.py
@@ -1,8 +1,19 @@
 # 5- Cria uma classe Carro e, depois, atribua valores aos atributos marca, modelo, ano e cor.
 
+# 6- Com base na classe Carro do exercício anterior, crie um método identificar que imprima “Meu carro é um modelo, da marca. Ele é do ano e tem a cor cor“.
+
+
+def metodo(self) -> None:
+    print(
+        f"""
+        Meu carro é um {self.modelo}, da {self.marca}.
+        Ele é do ano {self.ano} e tem a cor {self.cor}
+        """
+    )
+
 
 class Carro:
-    pass
+    fala_sobre_carro = metodo
 
 
 meu_carro = Carro()
@@ -10,3 +21,5 @@ meu_carro.marca = "Fiat"
 meu_carro.modelo = "Uno de escada"
 meu_carro.ano = 2000
 meu_carro.cor = "Vermelho"
+
+meu_carro.fala_sobre_carro()
