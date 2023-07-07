3abd888e60a5c579823537ce03553c4068db33c4
Frederico Tavares <fredericotp@hotmail.com>
2023-07-07 10:51:51 -0300

    Feat: Practice 2 - Tipagem

--- 5-eletiva-python/secao2-programacao-orientada-a-objetos/1-poo-em-python/practice/tipagem.py
+++ 5-eletiva-python/secao2-programacao-orientada-a-objetos/1-poo-em-python/practice/tipagem.py
@@ -1,7 +1,22 @@
 # 1- Modifique o código abaixo para que ele seja tipado corretamente.
 # def add_two_numbers(num1, num2):
-#    return num1 + num2
+#     return num1 + num2
 
 
 def add_two_numbers(num1: int, num2: int) -> int:
     return num1 + num2
+
+
+# 2- Modifique o código abaixo para que ele seja tipado corretamente.
+# class Pessoa:
+#     def __init__(self, nome, idade, altura):
+#         self.nome = nome
+#         self.idade = idade
+#         self.altura = altura
+
+
+class Pessoa:
+    def __init__(self, nome: str, idade: int, altura: float) -> None:
+        self.nome = nome
+        self.idade = idade
+        self.altura = altura
