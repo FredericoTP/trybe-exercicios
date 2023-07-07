76597ed941603e4a953fa517d37e2ceecc708d0d
Frederico Tavares <fredericotp@hotmail.com>
2023-07-07 13:50:30 -0300

    Feat: Exercise 1

---
+++ 5-eletiva-python/secao2-programacao-orientada-a-objetos/1-poo-em-python/exercises/exercise_1.py
@@ -0,0 +1,10 @@
+# Escreva uma função que recebe uma lista de inteiros e retorna a soma desses valores.
+
+
+def sum_list(numbers: list[int]) -> int:
+    return sum(numbers)
+
+
+valores = [1, 2, 3, 4, 5]
+
+print(sum_list(valores))
