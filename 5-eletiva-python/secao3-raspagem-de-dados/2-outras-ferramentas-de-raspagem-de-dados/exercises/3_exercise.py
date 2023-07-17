3ce35640f873a8a4e610e045a95595b3e17eaf77
Frederico Tavares <fredericotp@hotmail.com>
2023-07-17 19:04:12 -0300

    Feat: Exercise 3

---
+++ 5-eletiva-python/secao3-raspagem-de-dados/2-outras-ferramentas-de-raspagem-de-dados/exercises/3_exercise.py
@@ -0,0 +1,43 @@
+# Utilizando a ferramenta Selenium, no site https://diolinux.com.br/,
+# faça a extração dos campos título, link para o post, trecho exibido de cada post da página inicial.
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.add_argument("--headless")
+
+firefox = webdriver.Firefox(options=options)
+
+
+def scrape(url):
+    firefox.get(url)
+
+    posts = []
+
+    for post in firefox.find_elements(By.CLASS_NAME, "post-outer"):
+        new_item = dict()
+
+        new_item["title"] = (
+            post.find_element(By.CLASS_NAME, "entry-title")
+            .find_element(By.TAG_NAME, "a")
+            .get_attribute("innerHTML")
+        )
+
+        new_item["link"] = (
+            post.find_element(By.CLASS_NAME, "entry-title")
+            .find_element(By.TAG_NAME, "a")
+            .get_attribute("href")
+        )
+
+        new_item["excerpt"] = post.find_element(
+            By.CLASS_NAME, "entry-excerpt"
+        ).get_attribute("innerHTML")
+
+        posts.append(new_item)
+
+    return posts
+
+
+print(scrape("https://diolinux.com.br/"))
