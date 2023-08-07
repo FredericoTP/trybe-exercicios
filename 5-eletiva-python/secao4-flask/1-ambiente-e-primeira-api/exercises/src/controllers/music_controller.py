95ac99dad845c7734bd9c21571eae2a002391358
Frederico Tavares <fredericotp@hotmail.com>
2023-08-07 14:43:58 -0300

    Feat: Exercise 2

--- 5-eletiva-python/secao4-flask/1-ambiente-e-primeira-api/exercises/src/controllers/music_controller.py
+++ 5-eletiva-python/secao4-flask/1-ambiente-e-primeira-api/exercises/src/controllers/music_controller.py
@@ -10,3 +10,13 @@ def music_post():
     new_music = MusicModel(request.json)
     new_music.save()
     return jsonify(new_music.to_dict()), 201
+
+
+@music_controler.route("/random", methods=["GET"])
+def music_random():
+    music = MusicModel.get_random()
+
+    if music is None:
+        return jsonify({"error": "No musics available"}), 404
+
+    return jsonify(music.to_dict()), 200
