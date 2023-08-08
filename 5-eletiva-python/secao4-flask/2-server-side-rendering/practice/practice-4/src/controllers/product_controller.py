9d9af71a9cc2d131bdfa98fb623082fd4737d72b
Frederico Tavares <fredericotp@hotmail.com>
2023-08-08 13:58:49 -0300

    Feat: Practice 6

--- 5-eletiva-python/secao4-flask/2-server-side-rendering/practice/practice-4/src/controllers/product_controller.py
+++ 5-eletiva-python/secao4-flask/2-server-side-rendering/practice/practice-4/src/controllers/product_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from models.product_model import Product
 
 product_controller = Blueprint("product_controller", __name__)
@@ -9,3 +9,22 @@ products = []
 @product_controller.route("/", methods=["GET"])
 def index():
     return render_template("index.html", products=products)
+
+
+@product_controller.route("/", methods=["POST"])
+def add_product():
+    id = len(products) + 1
+    name = request.form["name"]
+    price = float(request.form["price"])
+    product = Product(id, name, price)
+    products.append(product)
+    return redirect("/")
+
+
+@product_controller.route("/delete/<int:product_id>")
+def delete_product(product_id):
+    for product in products:
+        if product.id == product_id:
+            products.remove(product)
+            break
+    return redirect("/")
