65e313f4024894c626ec98708fabd1ad905c35e1
Frederico Tavares <fredericotp@hotmail.com>
2023-08-10 12:41:27 -0300

    Feat: practice 3

--- 5-eletiva-python/secao4-flask/5-api-e-testes-em-flask/flask-project-manager/src/app.py
+++ 5-eletiva-python/secao4-flask/5-api-e-testes-em-flask/flask-project-manager/src/app.py
@@ -1,6 +1,6 @@
 # from os import environ
 # from waitress import serve
-from flask import Flask
+from flask import Flask, render_template
 from controllers.project_controller import project_controller
 
 app = Flask(__name__)
@@ -17,6 +17,11 @@ def start_server(host="0.0.0.0", port=8000):
     #     return app.run(debug=True, host=host, port=port)
     # else:
     #     serve(app, host=host, port=port)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("notFound.html"), 404
 
 
 if __name__ == "__main__":
