f02fd1a81674040e2086e935eeb50753eb42aa3d
Frederico Tavares <fredericotp@hotmail.com>
2023-08-17 14:42:10 -0300

    Feat: Exercise 3

--- 5-eletiva-python/secao5-django-1/2-templates-do-django/exercises/menu/urls.py
+++ 5-eletiva-python/secao5-django-1/2-templates-do-django/exercises/menu/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from menu.views import recipes
+from menu.views import recipes, recipe_details
 
-urlpatterns = [path("", recipes, name="recipes-page")]
+urlpatterns = [
+    path("", recipes, name="recipes-page"),
+    path("recipes/<int:recipe_id>", recipe_details, name="details-page"),
+]
--- 5-eletiva-python/secao5-django-1/2-templates-do-django/exercises/menu/views.py
+++ 5-eletiva-python/secao5-django-1/2-templates-do-django/exercises/menu/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from menu.models import Recipe
+from django.http import Http404
 
 
 # Create your views here.
 def recipes(request):
     context = {"recipes": Recipe.objects.all()}
     return render(request, "recipes.html", context)
+
+
+def recipe_details(request, recipe_id):
+    try:
+        recipe = get_object_or_404(Recipe, id=recipe_id)
+        return render(request, "recipe_details.html", {"recipe": recipe})
+    except Http404:
+        return render(request, "404.html")
