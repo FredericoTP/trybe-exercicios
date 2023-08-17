a2a988e8bfa3bcdd4dd2668741293b43ea08bd22
Frederico Tavares <fredericotp@hotmail.com>
2023-08-17 11:49:08 -0300

    Feat: Renderizando eventos no template

--- 5-eletiva-python/secao5-django-1/2-templates-do-django/event_manager/events/views.py
+++ 5-eletiva-python/secao5-django-1/2-templates-do-django/event_manager/events/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from events.models import Event
 
 
 # Create your views here.
 def index(request):
-    context = {"company": "Trybe"}
+    context = {"company": "Trybe", "events": Event.objects.all()}
     return render(request, "home.html", context)
 
 
