777eb840ac53effff4c79549d9abbf588d805287
Frederico Tavares <fredericotp@hotmail.com>
2023-08-17 13:24:38 -0300

    Feat: Template detalhes do evento

--- 5-eletiva-python/secao5-django-1/2-templates-do-django/event_manager/events/urls.py
+++ 5-eletiva-python/secao5-django-1/2-templates-do-django/event_manager/events/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from events.views import index, about
+from events.views import index, about, event_details
 
 urlpatterns = [
     path("", index, name="home-page"),
     path("about/", about, name="about-page"),
+    path("events/<int:event_id>", event_details, name="details-page"),
 ]
--- 5-eletiva-python/secao5-django-1/2-templates-do-django/event_manager/events/views.py
+++ 5-eletiva-python/secao5-django-1/2-templates-do-django/event_manager/events/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from events.models import Event
+from django.http import Http404
 
 
 # Create your views here.
@@ -10,3 +11,11 @@ def index(request):
 
 def about(request):
     return render(request, "about.html")
+
+
+def event_details(request, event_id):
+    try:
+        event = get_object_or_404(Event, id=event_id)
+        return render(request, "details.html", {"event": event})
+    except Http404:
+        return render(request, "404.html")
