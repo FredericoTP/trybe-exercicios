bcf3479e147ff7e7eac9573e1664c47b67a11743
Frederico Tavares <fredericotp@hotmail.com>
2023-08-18 11:58:20 -0300

    Feat: Enviando dados do template para a view e Practice 4

--- 5-eletiva-python/secao5-django-1/3-formularios-e-relacionamento-de-modelos/playlits_manager/playlists/views.py
+++ 5-eletiva-python/secao5-django-1/3-formularios-e-relacionamento-de-modelos/playlits_manager/playlists/views.py
@@ -1,15 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from playlists.forms import CreateMusicForm, CreateSingerForm
+from playlists.models import Music, Singer
 
 
 # Create your views here.
+def index(request):
+    context = {"musics": Music.objects.all(), "singers": Singer.objects.all()}
+    return render(request, "home.html", context)
+
+
 def music(request):
     form = CreateMusicForm()
+
+    if request.method == "POST":
+        form = CreateMusicForm(request.POST)
+
+        if form.is_valid():
+            Music.objects.create(**form.cleaned_data)
+            return redirect("home-page")
+
     context = {"form": form}
     return render(request, "music.html", context)
 
 
 def singer(request):
     form = CreateSingerForm()
+
+    if request.method == "POST":
+        form = CreateSingerForm(request.POST)
+
+        if form.is_valid():
+            Singer.objects.create(**form.cleaned_data)
+            return redirect("home-page")
+
     context = {"form": form}
     return render(request, "singer.html", context)
