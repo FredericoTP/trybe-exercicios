fa7162a5e6d581b3af9905d62459f7079a331dbb
Frederico Tavares <fredericotp@hotmail.com>
2023-08-18 10:38:54 -0300

    Feat: Criação de formulários

---
+++ 5-eletiva-python/secao5-django-1/3-formularios-e-relacionamento-de-modelos/playlits_manager/playlists/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+
+class CreateMusicForm(forms.Form):
+    name = forms.CharField(max_length=50)
+    recorded_at = forms.DateField()
+    length_in_seconds = forms.IntegerField()
+
+
+class CreatePlaylistForm(forms.Form):
+    name = forms.CharField(max_length=50)
+    is_active = forms.BooleanField()
