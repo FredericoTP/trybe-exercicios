38c79edbb63e1d9b151082b8cd87b80707ce1ff2
Frederico Tavares <fredericotp@hotmail.com>
2023-08-18 14:54:25 -0300

    Feat: Exercise 4

--- 5-eletiva-python/secao5-django-1/3-formularios-e-relacionamento-de-modelos/exercises/products/views.py
+++ 5-eletiva-python/secao5-django-1/3-formularios-e-relacionamento-de-modelos/exercises/products/views.py
@@ -1,3 +1,73 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from products.models import Product, Seller, Buyer, Order
+from products.forms import (
+    CreateBuyerForm,
+    CreateOrderForm,
+    CreateProductForm,
+    CreateSellerForm,
+)
+
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+
+def product(request):
+    form = CreateProductForm()
+
+    if request.method == "POST":
+        form = CreateProductForm(request.POST)
+
+        if form.is_valid():
+            Product.objects.create(**form.cleaned_data)
+            return redirect("home-page")
+
+    context = {"form": form}
+
+    return render(request, "product.html", context)
+
+
+def seller(request):
+    form = CreateSellerForm()
+
+    if request.method == "POST":
+        form = CreateSellerForm(request.POST)
+
+        if form.is_valid():
+            Seller.objects.create(**form.cleaned_data)
+            return redirect("home-page")
+
+    context = {"form": form}
+
+    return render(request, "seller.html", context)
+
+
+def buyer(request):
+    form = CreateBuyerForm()
+
+    if request.method == "POST":
+        form = CreateBuyerForm(request.POST)
+
+        if form.is_valid():
+            Buyer.objects.create(**form.cleaned_data)
+            return redirect("home-page")
+
+    context = {"form": form}
+
+    return render(request, "buyer.html", context)
+
+
+def order(request):
+    form = CreateOrderForm()
+
+    if request.method == "POST":
+        form = CreateOrderForm(request.POST)
+
+        if form.is_valid():
+            Order.objects.create(**form.cleaned_data)
+            return redirect("home-page")
+
+    context = {"form": form}
+
+    return render(request, "order.html", context)
