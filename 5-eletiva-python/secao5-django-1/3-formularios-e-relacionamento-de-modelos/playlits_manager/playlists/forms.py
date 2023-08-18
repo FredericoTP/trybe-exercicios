362027ffa018f4f02c82c1372f4925f26610d5c2
Frederico Tavares <fredericotp@hotmail.com>
2023-08-18 11:33:26 -0300

    Personalizando formulários

--- 5-eletiva-python/secao5-django-1/3-formularios-e-relacionamento-de-modelos/playlits_manager/playlists/forms.py
+++ 5-eletiva-python/secao5-django-1/3-formularios-e-relacionamento-de-modelos/playlits_manager/playlists/forms.py
@@ -3,9 +3,20 @@ from playlists.validators import validate_music_length, validate_name
 
 
 class CreateMusicForm(forms.Form):
-    name = forms.CharField(max_length=50)
-    recorded_at = forms.DateField()
-    length_in_seconds = forms.IntegerField(validators=[validate_music_length])
+    name = forms.CharField(
+        max_length=50,
+        validators={validate_name},
+        label="Nome da música",
+    )
+    recorded_at = forms.DateField(
+        label="Data da gravação",
+        initial="2023-07-06",
+        widget=forms.DateInput(attrs={"type": "date"}),
+    )
+    length_in_seconds = forms.IntegerField(
+        validators=[validate_music_length],
+        label="Duração em segundos",
+    )
 
 
 class CreatePlaylistForm(forms.Form):
