7eddec2a8f22231298a2092be2789618a01430db
Frederico Tavares <fredericotp@hotmail.com>
2023-08-21 11:35:48 -0300

    Feat: Api com Django Rest Framework - DRF

---
+++ 5-eletiva-python/secao5-django-1/5-django-rest-framework/django-rest-framework/core/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Singer, Music, Playlist
+
+# Register your models here.
+admin.site.register(Singer)
+admin.site.register(Music)
+admin.site.register(Playlist)
