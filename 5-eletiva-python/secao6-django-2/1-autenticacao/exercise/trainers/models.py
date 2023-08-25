426d4148ffdf329197aebbeba9f3c610503f90c2
Frederico Tavares <fredericotp@hotmail.com>
2023-08-25 13:22:32 -0300

    Feat: Exercise - Criação dos Models

--- 5-eletiva-python/secao6-django-2/1-autenticacao/exercise/trainers/models.py
+++ 5-eletiva-python/secao6-django-2/1-autenticacao/exercise/trainers/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=250)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class WorkoutPlan(models.Model):
+    client = models.OneToOneField(
+        Client,
+        on_delete=models.CASCADE,
+        related_name="workout_plan",
+    )
+    personal = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="workout_plan",
+    )
+    workout = models.TextField()
+
+    def __str__(self) -> str:
+        return f"{self.client} - Workout Plan"
