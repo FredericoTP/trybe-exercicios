ef615a8b160dbb4d25a8c10ab4c9420b0db492d4
Frederico <fredericotp@hotmail.com>
2025-08-25 18:31:59 -0300

    Trabalhando com várias promises

---
+++ 2.1-front-end/secao1-introducao-ao-frontend-e-javascript-assincrono/2-javascript-assincrono-promises-e-fetch/fetch/varios-fetch.js
@@ -0,0 +1,72 @@
+// função que gera número aleatório
+const generateRandomNumber = () => Math.round(Math.random() * 10);
+
+// cria uma promise que será sempre resolvida
+const generateResolvedPromise = (timer) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = generateRandomNumber();
+      console.log(`Promise resolvida em ${timer / 1000} segundo(s)`);
+      resolve(
+        `O número ${randomNumber} foi gerado em ${timer / 1000} segundo(s)`
+      );
+    }, timer);
+  });
+
+// cria uma promise que será sempre rejeitada
+const generateRejectedPromise = (timer) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = generateRandomNumber();
+      console.log(`Promise rejeitada em ${timer / 1000} segundo(s)`);
+      reject(
+        new Error(
+          `O número ${randomNumber} é inválido. Promise rejeitada em ${
+            timer / 1000
+          } segundo(s)`
+        )
+      );
+    }, timer);
+  });
+
+Promise.all([
+  generateResolvedPromise(1000),
+  generateResolvedPromise(3000),
+  generateResolvedPromise(2000),
+])
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => console.log(error.message));
+
+Promise.all([
+  generateResolvedPromise(1000),
+  generateResolvedPromise(3000),
+  generateRejectedPromise(2000),
+])
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => console.log(error.message));
+
+// Retorna a primeira promise resolvida
+Promise.race([
+  generateResolvedPromise(3000),
+  generateResolvedPromise(1000),
+  generateResolvedPromise(2000),
+])
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => console.log(error.message));
+
+// Retorna uma promise resolvida com o valor retornado pela primeira promise original, ignorando qualquer outra promise que tenha sido finalizada primeiro como rejeitada.
+Promise.any([
+  generateResolvedPromise(3000),
+  generateRejectedPromise(1000),
+  generateResolvedPromise(2000),
+])
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => console.log(error.message));
