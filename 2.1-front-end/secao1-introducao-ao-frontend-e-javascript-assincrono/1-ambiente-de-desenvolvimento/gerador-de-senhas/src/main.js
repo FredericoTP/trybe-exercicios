41fd152148f42f5cff17e8ac4eca1a661f42a9b2
Frederico <fredericotp@hotmail.com>
2025-08-25 13:16:50 -0300

    Exercício de fixação

--- 2.1-front-end/secao1-introducao-ao-frontend-e-javascript-assincrono/1-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
+++ 2.1-front-end/secao1-introducao-ao-frontend-e-javascript-assincrono/1-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
@@ -1,4 +1,5 @@
 import {nanoid} from "nanoid"
+import copy from 'clipboard-copy';
 import "./style.css"
 
 const passwordBtnEl = document.querySelector('button');
@@ -7,4 +8,9 @@ const displayPasswordEL = document.querySelector('h2');
 passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEL.innerHTML = randomPassword;
+});
+
+displayPasswordEL.addEventListener('click', () => {
+  copy(event.target.innerHTML);
+  alert("Senha copiada!");
 });
